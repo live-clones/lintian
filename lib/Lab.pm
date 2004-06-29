1de4a305317300b0817cdbf21dd5d47129c05820
Jeroen van Wolffelaar <jeroen@debian.org>
2004-06-29 15:24:12 +0000

    Revert changes to frontend/lintian and lib/Lab.pm that were accidentally committed in r299

--- lib/Lab.pm
+++ lib/Lab.pm
@@ -1,7 +1,7 @@
 # Lab -- Perl laboratory functions for lintian
 # $Id$
 
-# Copyright (C) 2004 Jeroen van Wolffelaar
+# Copyright (C) 1998-2004 Various authors
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,7 +23,6 @@ package Lab;
 use strict;
 
 use Pipeline;
-use File::Temp ( tempdir );
 
 my $LINTIAN_ROOT = $::LINTIAN_ROOT;
 
@@ -32,30 +31,66 @@ my $LINTIAN_ROOT = $::LINTIAN_ROOT;
 my $verbose = $::verbose;
 my $debug = $::debug;
 
-my $tempdir = undef;
+sub is_lab {
+	my $labdir = shift;
 
-sub unpack {
+	return -d "$labdir/binary"
+		&& -d "$labdir/udeb"
+		&& -d "$labdir/source"
+		&& -d "$labdir/info";
+}
 
-	unless (defined $tempdir) {
-		$tempdir = tempdir("lintian.XXXXXX",
-			TMPDIR => 1, CLEANUP => 1) or
-			die("Couldn't create temporary directory for examining
-			package(s)");
-	}
+sub setup {
+	my $LINTIAN_LAB = shift;
+	my $lab_mode = shift;
 
-	my ($type, $package) = @_;
+    print "N: Setting up lab in $LINTIAN_LAB ...\n" if $verbose;
 
-	if ($type eq 'b' || $type eq 'u') {
-		spawn("$LINTIAN_ROOT/unpack/unpack-binpkg-l1", $tempdir, $file) == 0
-			or die("Failed unpacking $file to level 1");
-		spawn("$LINTIAN_ROOT/unpack/unpack-binpkg-l2", $tempdir, $file) == 0
-			or die("Failed unpacking $file to level 2");
-	} else {
-		spawn("$LINTIAN_ROOT/unpack/unpack-srcpkg-l1", $tempdir, $file) == 0
-			or die("Failed unpacking $file to level 1");
-		spawn("$LINTIAN_ROOT/unpack/unpack-srcpkg-l2", $tempdir, $file) == 0
-			or die("Failed unpacking $file to level 2");
-	}
+    # create lab directory
+    if (not -d "$LINTIAN_LAB" or ($lab_mode eq 'temporary')) {
+		# (Note, that the mode 0777 is reduced by the current umask.)
+		mkdir($LINTIAN_LAB,0777) or fail("cannot create lab directory $LINTIAN_LAB");
+    }
+
+    # create base directories
+    if (not -d "$LINTIAN_LAB/binary") {
+		mkdir("$LINTIAN_LAB/binary",0777) or fail("cannot create lab directory $LINTIAN_LAB/binary");
+    }
+    if (not -d "$LINTIAN_LAB/source") {
+		mkdir("$LINTIAN_LAB/source",0777) or fail("cannot create lab directory $LINTIAN_LAB/source");
+    }
+    if (not -d "$LINTIAN_LAB/udeb") {
+		mkdir("$LINTIAN_LAB/udeb",0777) or fail("cannot create lab directory $LINTIAN_LAB/udeb");
+    }
+    if (not -d "$LINTIAN_LAB/info") {
+		mkdir("$LINTIAN_LAB/info",0777) or fail("cannot create lab directory $LINTIAN_LAB/info");
+    }
+	# just create empty files
+	_touch("$LINTIAN_LAB/info/binary-packages")
+		or fail("cannot create binary package list");
+	_touch("$LINTIAN_LAB/info/source-packages")
+		or fail("cannot create source package list");
+	_touch("$LINTIAN_LAB/info/udeb-packages")
+		or fail("cannot create udeb package list");
+}
+
+sub populate_with_dist {
+    my $LINTIAN_LAB = shift;
+    my $LINTIAN_DIST = shift;
+
+	print STDERR "spawning list-binpkg, list-udebpkg and list-srcpkg since LINTIAN_DIST=$LINTIAN_DIST\n" if ($debug >= 2);
+
+	my $v = $verbose ? '-v' : '';
+
+	spawn("$LINTIAN_ROOT/unpack/list-binpkg",
+		  "$LINTIAN_LAB/info/binary-packages", $v) == 0
+		  or fail("cannot create binary package list");
+	spawn("$LINTIAN_ROOT/unpack/list-srcpkg",
+		  "$LINTIAN_LAB/info/source-packages", $v) == 0
+		  or fail("cannot create source package list");
+	spawn("$LINTIAN_ROOT/unpack/list-udebpkg",
+		  "$LINTIAN_LAB/info/udeb-packages", $v) == 0
+		  or fail("cannot create udeb package list");
 }
 
 # Remove is apparantly some reserved name...
@@ -109,6 +144,16 @@ sub delete {
 		}
     }
 }
+
+# create an empty file
+# --okay, okay, this is not exactly what `touch' does :-)
+sub _touch {
+    open(T,">$_[0]") or return 0;
+    close(T) or return 0;
+
+    return 1;
+}
+
 
 1;
 
