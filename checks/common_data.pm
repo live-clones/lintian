e87943adf293762df9ccebb20f67c1cbc2b580ae
Russ Allbery <rra@debian.org>
2007-10-14 23:20:40 +0000

    * checks/common_data.pm:   + [RA] Allow Homepage in source and binary packages.

--- checks/common_data.pm
+++ checks/common_data.pm
@@ -84,15 +84,15 @@ use vars qw
     ('source', 'version', 'maintainer', 'binary', 'architecture',
      'standards-version', 'files', 'build-depends', 'build-depends-indep',
      'build-conflicts', 'build-conflicts-indep', 'format', 'origin',
-     'uploaders', 'python-version', 'autobuild', 'vcs-arch', 'vcs-bzr',
-     'vcs-cvs', 'vcs-darcs', 'vcs-git', 'vcs-hg', 'vcs-mtn', 'vcs-svn',
-     'vcs-browser');
+     'uploaders', 'python-version', 'autobuild', 'homepage', 'vcs-arch',
+     'vcs-bzr', 'vcs-cvs', 'vcs-darcs', 'vcs-git', 'vcs-hg', 'vcs-mtn',
+     'vcs-svn', 'vcs-browser');
 
 %known_binary_fields = map { $_ => 1 }
     ('package', 'version', 'architecture', 'depends', 'pre-depends',
      'recommends', 'suggests', 'enhances', 'conflicts', 'provides',
      'replaces', 'essential', 'maintainer', 'section', 'priority',
-     'source', 'description', 'installed-size', 'python-version');
+     'source', 'description', 'installed-size', 'python-version', 'homepage');
 
 %known_udeb_fields = map { $_ => 1 }
     ('package', 'version', 'architecture', 'subarchitecture', 'depends',
