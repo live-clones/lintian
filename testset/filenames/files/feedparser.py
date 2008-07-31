893e4531a64d388953bcea5d5dccd2ca2dd762fd
Chris Lamb <chris@chris-lamb.co.uk>
2008-07-31 19:41:01 +0100

    Emit a warning when a package embeds Mark Pilgrim's feedparser.py.
    We check the contents of the file first to avoid some false positives,
    particularly /usr/lib/python2.5/email/feedparser.py.

---
+++ testset/filenames/files/feedparser.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""Universal feed parser
+
+Handles RSS 0.9x, RSS 1.0, RSS 2.0, CDF, Atom 0.3, and Atom 1.0 feeds
+
+Visit http://feedparser.org/ for the latest version
+Visit http://feedparser.org/docs/ for the latest documentation
+
+Required: Python 2.1 or later
+Recommended: Python 2.3 or later
+Recommended: CJKCodecs and iconv_codec <http://cjkpython.i18n.org/>
+"""
+
+__version__ = "4.1"# + "$Revision: 1.92 $"[11:15] + "-cvs"
+
+# (snip)
