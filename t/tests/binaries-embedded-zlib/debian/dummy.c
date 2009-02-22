ba0aa594881055f6d229e6f45f9601846f6d7c3d
Russ Allbery <rra@debian.org>
2009-02-22 13:23:49 -0800

    Use a less artificial test for embedded-zlib

---
+++ t/tests/binaries-embedded-zlib/debian/dummy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * zlib asks derivative works to include this string, so it's the signature
+ * that we use to detect embedded copies.
+ */
+static const char deflate_copyright[]
+    = "deflate 1.2.3.3 Copyright 1995-2006 Jean-loup Gailly";
+
+int
+main(void)
+{
+    printf("%s\n", deflate_copyright);
+}
