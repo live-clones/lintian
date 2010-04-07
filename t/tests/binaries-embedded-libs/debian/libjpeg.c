52ca0ed2c94f597afa227a2e508f0cc0f6e03ab5
Raphael Geissert <atomo64@gmail.com>
2010-04-07 13:50:17 -0500

    Detect embedded copies of lib{,open}jpeg
    Again, detect them based on a rather unique error string.
    
    * checks/binaries{,.desc}:
      + [RG] Detect embedded copies of lib{,open}jpeg.

---
+++ t/tests/binaries-embedded-libs/debian/libjpeg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * The quantization tables warning message is unique enough to be used to
+ * detect embedded or statically-linked copies of libjpeg.
+ */
+static const char quantization_tables_warning[]
+    = "Caution: quantization tables are too coarse for baseline JPEG";
+
+int
+main(void)
+{
+    printf("%s\n", quantization_tables_warning);
+}
---
+++ t/tests/binaries-embedded-libs/debian/libopenjpeg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * The tcd_decode error message appears to be unique enough to be used to
+ * detect embedded or statically-linked copies of libopenjpeg.
+ */
+static const char tcd_error[]
+    = "tcd_decode: incomplete bistream";
+
+int
+main(void)
+{
+    printf("%s\n", tcd_error);
+}
