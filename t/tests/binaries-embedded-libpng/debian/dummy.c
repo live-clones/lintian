6fbcbd47e94db7721812c7b5c40ebfc8315e80c2
Raphael Geissert <atomo64@gmail.com>
2010-04-07 13:07:32 -0500

    Detect embedded copies of libpng
    Detect them based on a rather unique error string.
    
    * checks/binaries{,.desc}:
      + [RG] Detect embedded copies of libpng.

---
+++ t/tests/binaries-embedded-libpng/debian/dummy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * The png_zalloc overflow error message is unique enough to be used to
+ * detect embedded or statically-linked copies of libpng.
+ */
+static const char zalloc_error[]
+    = "Potential overflow in png_zalloc()";
+
+int
+main(void)
+{
+    printf("%s\n", zalloc_error);
+}
