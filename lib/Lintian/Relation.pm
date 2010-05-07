aed71d1b04b02e0c028a8e8b3612864fec0cc273
Russ Allbery <rra@debian.org>
2010-05-06 21:57:52 -0700

    Allow a mix of package name and substvar in relations
    * lib/Lintian/Relation.pm:
      + [RA] Rather than allowing only a package name or a substvar, allow
        substvars mixed into the package name.  (Closes: #580494)

--- lib/Lintian/Relation.pm
+++ lib/Lintian/Relation.pm
@@ -75,9 +75,11 @@ sub parse_element {
     $element =~ /
         ^\s*                            # skip leading whitespace
         (                               # package name or substvar (1)
-         [a-zA-Z0-9][a-zA-Z0-9+.-]+     #   package name
-         |                              #   or
-         \$\{[a-zA-Z0-9:-]+\}           #   substvar
+         (?:                            #  substvars may be mixed in
+          [a-zA-Z0-9][a-zA-Z0-9+.-]+    #   package name
+          |                             #   or
+          \$\{[a-zA-Z0-9:-]+\}          #   substvar
+         )+                             #  one or more name or substvar
         )                               # end of package name or substvar
         (?:                             # start of optional version
          \s* \(                         # open parenthesis for version part
