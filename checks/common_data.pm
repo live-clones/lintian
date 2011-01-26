c19067fe6c79656154ed1d19ab01a6f1d57a28e1
Niels Thykier <niels@thykier.net>
2011-01-26 09:49:07 +0100

    Moved "known_source_fields" into a data file

--- checks/common_data.pm
+++ checks/common_data.pm
@@ -1,30 +1,22 @@
-#! /usr/bin/perl -w
-
 package common_data;
+
+use strict;
+use warnings;
+
 use base qw(Exporter);
 
 our @EXPORT = qw
 (
-   %known_source_fields $known_shells_regex
+   $known_shells_regex
 );
 
 # To let "perl -cw" test know we use these variables;
 use vars qw
 (
-  %known_source_fields $known_shells_regex
+  $known_shells_regex
 );
 
 # simple defines for commonly needed data
-
-# The Ubuntu original-maintainer field is handled separately.
-%known_source_fields = map { $_ => 1 }
-    ('source', 'version', 'maintainer', 'binary', 'architecture',
-     'standards-version', 'files', 'build-depends', 'build-depends-indep',
-     'build-conflicts', 'build-conflicts-indep', 'format', 'origin',
-     'uploaders', 'python-version', 'autobuild', 'homepage', 'vcs-arch',
-     'vcs-bzr', 'vcs-cvs', 'vcs-darcs', 'vcs-git', 'vcs-hg', 'vcs-mtn',
-     'vcs-svn', 'vcs-browser', 'dm-upload-allowed', 'bugs', 'checksums-sha1',
-     'checksums-sha256', 'checksums-md5');
 
 $known_shells_regex = qr'(?:(?:b|d)?a|t?c|(?:pd|m)?k|z)?sh';
 
