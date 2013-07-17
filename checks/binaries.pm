c8208ba825363406684ca01aa9953a4774d2adb3
Jakub Wilk <jwilk@debian.org>
2013-07-17 14:38:13 +0200

    c/binaries.pm: Add missing escape of "." in regex
    Signed-off-by: Niels Thykier <niels@thykier.net>

--- checks/binaries.pm
+++ checks/binaries.pm
@@ -361,7 +361,7 @@ foreach my $file ($info->sorted_index) {
     }
 
     # Something other than detached debugging symbols in /usr/lib/debug paths.
-    if ($file =~ m,^usr/lib/debug/(?:lib\d*|s?bin|usr|opt|dev|emul|.build-id)/,) {
+    if ($file =~ m,^usr/lib/debug/(?:lib\d*|s?bin|usr|opt|dev|emul|\.build-id)/,) {
         if (scalar (@{ $objdump->{NEEDED} }) ) {
             tag 'debug-file-should-use-detached-symbols', $file;
         }
