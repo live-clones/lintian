7888e2ea6dde6aa8a878fc05abfb6b8681e61f75
Bastien ROUCARIÈS <roucaries.bastien+debian@gmail.com>
2015-08-03 21:47:03 +0200

    Fix a test suite failure with gcc5
    Fix it by:
    - using compat 9
    - removing gets() that does not exist with C11 (used by default
     by gcc5).
    
    Signed-off-by: Bastien ROUCARIÈS <roucaries.bastien+debian@gmail.com>

--- t/tests/binaries-general/debian/basic.c
+++ t/tests/binaries-general/debian/basic.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int
-main(void)
+main(int argc, char *argv[])
 {
     char t[10];
     printf("Hello world!\n");
-    /* Bad choice for reading from stdin, but it forces a stack
+    /* Bad choice for reading from a security point of view, but it forces a stack
        protector, so meh.
      */
-    gets (t);
+    if(argc > 0)
+       (void) strcpy(t,argv[0]);
 }
