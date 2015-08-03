02389b1e7d24f2ef632ac790557eb2137d912922
Bastien ROUCARIÈS <roucaries.bastien+debian@gmail.com>
2015-08-03 22:46:02 +0200

    Closes serious bug and improve test
    return strcpy result in order to avoid code deletion by optimizer
    
    Signed-off-by: Bastien ROUCARIÈS <roucaries.bastien+debian@gmail.com>

--- t/tests/binaries-general/debian/basic.c
+++ t/tests/binaries-general/debian/basic.c
@@ -6,9 +6,7 @@ main(int argc, char *argv[])
 {
     char t[10];
     printf("Hello world!\n");
-    /* Bad choice for reading from a security point of view, but it forces a stack
-       protector, so meh.
-     */
-    if(argc > 0)
-       (void) strcpy(t,argv[0]);
+    /* forces a stack protector */
+    (void) strcpy(t,argv[0]);
+    return (int) t[0];
 }
