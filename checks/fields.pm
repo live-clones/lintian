d1e02f433542352a86c9e83a108bb472d4892527
Jakub Wilk <jwilk@debian.org>
2016-04-04 20:36:44 +0200

    checks/fields: Relax build profile dep checks to allow ~
    The analogous change to checks/control-file was made in
    78783f2338fa864f2ed9dd76fb212555e23000ee.

--- checks/fields.pm
+++ checks/fields.pm
@@ -1127,13 +1127,13 @@ sub run {
         if ($restrictions_used) {
             my $build_conflicts_all = $info->relation('build-conflicts-all');
             tag 'restriction-formula-without-versioned-dpkg-dev-dependency'
-              unless ($build_all->implies('dpkg-dev (>= 1.17.14)'));
+              unless ($build_all->implies('dpkg-dev (>= 1.17.14~)'));
             # if the package uses debhelper then it must require and not
             # conflict with version >= 9.20141010
             if ($build_all->implies('debhelper')) {
                 tag
 'restriction-formula-with-debhelper-without-debhelper-version'
-                  unless ($build_all->implies('debhelper (>= 9.20141010)'));
+                  unless ($build_all->implies('debhelper (>= 9.20141010~)'));
             }
         }
 
