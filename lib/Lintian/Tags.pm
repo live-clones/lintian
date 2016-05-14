live-clones/lintian

commit c06bbf0b68cd5c18508d955de60b8540c010df27

Move code for "unused-override" into L::Tags
Signed-off-by: Niels Thykier <niels@thykier.net>

lib/Lintian/Tags.pm
@@ -648,17 +648,61 @@ sub file_overrides {
     return;
 }
 
+=item load_overrides
+
+Loads overrides for the current file.  This is basically a short-hand
+for finding the overrides file in the lab and calling
+L<files_overrides|/file_overrides(OVERRIDE-FILE)> on it if it is
+present.
+
+=cut
+
+sub load_overrides {
+    my ($self, $overrides) = @_;
+    my $current = $self->{current};
+    my $lpkg;
+    my $overrides_file;
+    unless (defined($current)) {
+        die 'no current file when loading overrides';
+    }
+    $lpkg = $self->{'info'}{$current}{'processable'};
+    $overrides_file = $lpkg->info->lab_data_path('override');
+    if ( -f $overrides_file ) {
+        $self->file_overrides($overrides_file);
+    }
+    return;
+}
+
 =item file_end()
 
-Ends processing of a file.  The main reason for this call is to, in turn,
-call Lintian::Output::print_end_pkg() to mark the end of the package.
+Ends processing of a file.
+
+This does two things.  First it emits "unused-override" tags for all
+unused overrides.  Secondly, it calls Lintian::Output::print_end_pkg
+to mark the end of the package.
+
+Note that this method is called by file_start if it detects another
+entry is already active.
 
 =cut
 
 sub file_end {
     my ($self) = @_;
-    if ($self->{current}) {
-        my $info = $self->{info}{$self->{current}};
+    if (my $current = $self->{current}) {
+        my $info = $self->{info}{$current};
+        my $pkg_overrides = $info->{overrides};
+
+        for my $tag (sort(keys %{$pkg_overrides} )) {
+            my $overrides;
+            next if $self->suppressed($tag);
+
+            $overrides = $pkg_overrides->{$tag};
+            for my $extra (sort(keys %{$overrides} )) {
+                next if $overrides->{$extra};
+                $self->tag('unused-override', $tag, $extra);
+            }
+        }
+
         $Lintian::Output::GLOBAL->print_end_pkg($info);
     }
     undef $self->{current};

frontend/lintian
@@ -1187,7 +1187,6 @@ foreach my $gname (sort $pool->get_group_names()) {
 
 # Write the lab state to the disk, so it remembers the new packages
 $LAB->close;
-$TAGS->file_end();
 
 if ($action eq 'check' and not $opt{'no-override'} and not $opt{'show-overrides'}) {
     my $errors = $overrides{errors} || 0;
@@ -1267,19 +1266,6 @@ sub auto_clean_package {
 
 sub post_pkg_process_overrides{
     my ($lpkg) = @_;
-    # report unused overrides
-    if (not $opt{'no-override'}) {
-        my $pkg_overrides = $TAGS->overrides ($lpkg);
-
-        for my $tag (sort keys %$pkg_overrides) {
-            next if $TAGS->suppressed($tag);
-
-            for my $extra (sort keys %{$pkg_overrides->{$tag}}) {
-                next if $pkg_overrides->{$tag}{$extra};
-                tag ('unused-override', $tag, $extra);
-            }
-        }
-    }
 
     # Report override statistics.
     if (not $opt{'no-override'} and not $opt{'show-overrides'}) {
@@ -1400,11 +1386,9 @@ sub process_group {
 
         debug_msg(1, "Base directory in lab: $base");
 
-        unless ($opt{'no-override'}) {
-            if ($collmap->getp ('override-file') && -f "$base/override") {
-                debug_msg(1, 'Override file collected, loading it ...');
-                $TAGS->file_overrides("$base/override");
-            }
+        if (not $opt{'no-override'} and $collmap->getp('override-file')) {
+            debug_msg(1, 'Loading overrides file (if any) ...');
+            $TAGS->load_overrides;
         }
         foreach my $script (@scripts) {
             my $cs = $PROFILE->get_script ($script);
@@ -1442,6 +1426,8 @@ sub process_group {
         }
         post_pkg_process_overrides ($lpkg);
     } # end foreach my $lpkg ($group->get_processable)
+
+    $TAGS->file_end;
 
     if ($debug > 2) {
         for my $lpkg ($group->get_processables) {

commit 8dda8da3a4c97303f1a40a69fbd3a554344d5bbe

L::Tags: Always emit unused-overrides
This appears to have been a regression in c06bbf0.

Signed-off-by: Niels Thykier <niels@thykier.net>

lib/Lintian/Tags.pm
@@ -487,8 +487,7 @@ sub file_start {
         overrides => {},
     };
     if ($self->{current}) {
-        my $info = $self->{info}{$self->{current}};
-        $Lintian::Output::GLOBAL->print_end_pkg($info);
+        $self->file_end;
     }
     $self->{current} = $file;
     $Lintian::Output::GLOBAL->print_start_pkg($self->{info}{$file});