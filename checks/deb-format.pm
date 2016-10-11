live-clones/lintian

commit 6a369c4914a19bad6d15bcc76dd370df43cb5a0e

c/deb-format: Replace a use of spawn with safe_qx
Signed-off-by: Niels Thykier <niels@thykier.net>

checks/deb-format.pm
@@ -22,7 +22,7 @@ use autodie;
 
 use List::MoreUtils qw(first_index none);
 
-use Lintian::Command qw(spawn);
+use Lintian::Command qw(safe_qx spawn);
 use Lintian::Data;
 use Lintian::Tags qw(tag);
 
@@ -195,12 +195,11 @@ sub run {
     # supports a newer format but it's not permitted in the archive
     # yet.
     if (not defined($failed)) {
-        $opts = {};
-        $success = spawn($opts, ['ar', 'p', $deb, 'debian-binary']);
-        if (not $success) {
+        my $output = spawn('ar', 'p', $deb, 'debian-binary');
+        if ($? != 0) {
             tag 'malformed-deb-archive', 'cannot read debian-binary member';
-        } elsif (${ $opts->{out} } !~ /^2\.\d+\n/) {
-            my ($version) = split("\n", ${ $opts->{out} });
+        } elsif ($output !~ /^2\.\d+\n/) {
+            my ($version) = split(m/\n/, $output);
             tag 'malformed-deb-archive', "version $version not 2.0";
         }
     }

commit 1588489ae5e44700668104eb1d780b6194664ed9

checks/deb-format: Actually use safe_qx
Fixes: 6a369c4914a19bad6d15bcc76dd370df43cb5a0e

checks/deb-format.pm
@@ -195,7 +195,7 @@ sub run {
     # supports a newer format but it's not permitted in the archive
     # yet.
     if (not defined($failed)) {
-        my $output = spawn('ar', 'p', $deb, 'debian-binary');
+        my $output = safe_qx('ar', 'p', $deb, 'debian-binary');
         if ($? != 0) {
             tag 'malformed-deb-archive', 'cannot read debian-binary member';
         } elsif ($output !~ /^2\.\d+\n/) {