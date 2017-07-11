7319953bad3ae5e0e15f778a7ed19dd20241b77c
Chris Lamb <lamby@debian.org>
2017-07-11 22:43:30 +0100

    [CL] Check for "FIXME" and similar placeholders in various files under debian/. (Closes: #846009)

--- checks/cruft.pm
+++ checks/cruft.pm
@@ -707,6 +707,16 @@ sub find_cruft {
             }
         }
 
+        if (   $name =~ m{^debian/(README.source|copyright|rules|control)}
+            && $entry->is_file
+            && $entry->is_open_ok) {
+            my $fd = $entry->open;
+            while (my $line = <$fd>) {
+                next unless $line =~ m/(FIX_?ME)/;
+                tag 'file-contains-fixme-placeholder', "$name:$. $1";
+            }
+        }
+
         # Find mentioning of usr/lib/perl5 inside the packaging
         if ($name =~ m{^debian/}) {
             my $short = $entry->basename;
