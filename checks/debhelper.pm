d78d475e6cdf000fcb879dc3a2788773c7536917
Chris Lamb <lamby@debian.org>
2017-07-24 22:41:42 +0100

    Prevent a false positive of missing-build-dependency-for-dh_-command that can be exposed by following the advice for the recently added useless-autoreconf-build-depends tag. (Closes: #869541)

--- checks/debhelper.pm
+++ checks/debhelper.pm
@@ -433,6 +433,7 @@ sub run {
 
     while (my ($dep, $command) = each %missingbdeps) {
         next if $dep eq 'debhelper'; #handled above
+        next if $dep eq 'autotools-dev' and $level >= 10;
         tag 'missing-build-dependency-for-dh_-command', "$command => $dep"
           unless ($bdepends_noarch->implies($dep));
     }
