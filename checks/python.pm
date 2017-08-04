4ce907fb43a60eb6b8ddd9b31b906e9ed4f9605b
Chris Lamb <lamby@debian.org>
2017-08-04 11:07:25 -0400

    Check for packages that Build-Depend on python-sphinx only. (Closes: #870730)

--- checks/python.pm
+++ checks/python.pm
@@ -46,6 +46,11 @@ sub run {
         }
     }
 
+    my $build_all = $info->relation('build-depends-all');
+    tag 'build-depends-on-python-sphinx-only'
+      if $build_all->implies('python-sphinx')
+      and !$build_all->implies('python3-sphinx');
+
     return;
 }
 
