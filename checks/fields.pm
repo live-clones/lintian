b2c73b56774505439b774e256616212102a0f804
Chris Lamb <lamby@debian.org>
2017-11-20 08:55:49 +0900

    Only warn about well-known insecure URIs in homepage-field-uses-insecure-uri.

--- checks/fields.pm
+++ checks/fields.pm
@@ -629,7 +629,10 @@ sub run {
         if ($homepage=~ m,bioconductor\.org/packages/.*/bioc/html/.*\.html*$,){
             tag 'homepage-for-bioconductor-package-not-canonical', $orig;
         }
-        if ($homepage=~ m,^http:,){
+        if (   $homepage =~ m,^ftp//,
+            or $homepage =~
+            =~ m,^http://(?:[^\.]+\.)?(?:github\.com|metacpan\.org|debian\.org)/,
+          ){
             tag 'homepage-field-uses-insecure-uri', $orig;
         }
     } elsif (not $info->native) {
