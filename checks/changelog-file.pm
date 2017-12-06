f4568845d3e649b03261119a1244e485f79b9209
Chris Lamb <lamby@debian.org>
2017-12-06 20:10:06 +0000

    Correct operator precedence in "epoch-change-without-comment" to prevent a false positive when an epoch is present but is unchanged between versions.

--- checks/changelog-file.pm
+++ checks/changelog-file.pm
@@ -346,7 +346,7 @@ sub run {
 
             my ($first_epoch) = $first_version =~ /^([^:]+):/;
             my ($second_epoch) = $second_version =~ /^([^:]+):/;
-            if ($first_epoch and $second_epoch // '' ne $first_epoch) {
+            if ($first_epoch and ($second_epoch // '') ne $first_epoch) {
                 tag 'epoch-change-without-comment',
                   unless $entries[0]->Changes =~ /\bepoch\b/im;
             }
