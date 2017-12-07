8d58d79e05d4719394db69dc1d8ad98bdb33289d
Chris Lamb <lamby@debian.org>
2017-12-07 09:49:00 +0000

    Correct false positives in the "source-includes-file-in-files-excluded" tag where a Files-Excluded of "lib/*" would be triggered for "foolib/filename".

--- checks/source-copyright.pm
+++ checks/source-copyright.pm
@@ -632,7 +632,7 @@ sub check_files_excluded {
             $wc_value = qr/^${wc_value}(?:\/|$)/;
         }
         for my $srcfile (@files) {
-            if ($srcfile =~ $wc_value) {
+            if ($srcfile =~ qr/^$wc_value/) {
                 tag 'source-includes-file-in-files-excluded',$srcfile;
                 last;
             }
