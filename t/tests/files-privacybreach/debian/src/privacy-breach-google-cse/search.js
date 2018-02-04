aaf6a73f04718e61f56fbc79b4ec6613ec4c2078
Bastien ROUCARIÈS <roucaries.bastien+debian@gmail.com>
2018-02-04 22:36:55 +0100

    Detect new fragment for google cse
    Signed-off-by: Bastien ROUCARIÈS <roucaries.bastien+debian@gmail.com>

---
+++ t/tests/files-privacybreach/debian/src/privacy-breach-google-cse/search.js
@@ -0,0 +1,14 @@
+<!-- http://www.google.com/cse/manage/all -->
+
+<script type="text/javascript">
+  (function() {
+    var cx = '006134137889097767902:turn9fku95u';
+    var gcse = document.createElement('offline-script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('offline-script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
