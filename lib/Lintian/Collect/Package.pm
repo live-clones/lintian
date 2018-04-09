00a4b17ed3519541b2c35e8ccd360e2665a65100
Chris Lamb <lamby@debian.org>
2018-04-09 08:50:44 +0100

    lib/Lintian/Collect/Package.pm: Also allow for links, etc. in permission field.

--- lib/Lintian/Collect/Package.pm
+++ lib/Lintian/Collect/Package.pm
@@ -444,8 +444,7 @@ sub _fetch_index_data {
         # Note this cannot ever be 100% reliable as the filename might contain
         # "fake" dates.
         ($perm,$ownership,$size,$date,$time,$name)
-          = $line
-          =~ /^([-drwx]{10}) (.*?) (\d+) ([-\d]{10}) ([:\d]{5,8}) (.*)$/;
+          = $line=~ /^(.{10}) (.*?) (\d+) ([-\d]{10}) ([:\d]{5,8}) (.*)$/;
         $ownership =~ s/\s+$//;
 
         $file{'date_time'} = "${date} ${time}";
