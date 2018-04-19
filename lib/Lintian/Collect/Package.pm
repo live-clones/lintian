live-clones/lintian

commit e0c833b30d5cc59e5b5e8c63dfb5ec741e83c5f6

L::C::*: Refactor index methods and correct "anchored root"
Signed-off-by: Niels Thykier <niels@thykier.net>

lib/Lintian/Collect/Package.pm
@@ -283,8 +283,19 @@ Needs-Info requirements for using I<index>: unpacked
 
 sub index {
     my ($self, $file) = @_;
-    return $self->_fetch_index_data('index', 'index', 'index-owner-id',
-        'unpacked', $file);
+    if (my $cache = $self->{'index'}) {
+        return $cache->{$file}
+          if exists($cache->{$file});
+        return;
+    }
+    my $load_info = {
+        'field' => 'index',
+        'index_file' => 'index',
+        'index_owner_file' => 'index-owner-id',
+        'fs_root_sub' => 'unpacked',
+        'has_anchored_root_dir' => 1,
+    };
+    return $self->_fetch_index_data($load_info, $file);
 }
 
 =item sorted_index
@@ -387,23 +398,19 @@ sub _fetch_extracted_dir {
 # Backing method for index and others; this is not a part of the API.
 # sub _fetch_index_data Needs-Info none
 sub _fetch_index_data {
-    my ($self, $field, $index, $indexown, $path_sub, $file) = @_;
-    if (my $cache = $self->{$field}) {
-        return $cache->{$file}
-          if exists $cache->{$file};
-        return;
-    }
+    my ($self, $load_info, $file) = @_;
+
+    my (%idxh, %children, $num_idx, %rhlinks, @sorted);
     my $base_dir = $self->base_dir;
-    my (%idxh, %children);
-    my $num_idx;
-    my %rhlinks;
-    my @sorted;
     local $_;
+    my $field = $load_info->{'field'};
+    my $index = $load_info->{'index_file'};
+    my $indexown = $load_info->{'index_owner_file'};
     my $idx = open_gz("$base_dir/${index}.gz");
     my $fs_info = Lintian::Path::FSInfo->new(
         '_collect' => $self,
-        '_collect_path_sub' => $path_sub,
-        'has_anchored_root_dir' => 1,
+        '_collect_path_sub' => $load_info->{'fs_root_sub'},
+        'has_anchored_root_dir' => $load_info->{'as_anchored_root_dir'},
     );
 
     if ($indexown) {

lib/Lintian/Collect/Binary.pm
@@ -197,8 +197,21 @@ Needs-Info requirements for using I<control_index>: bin-pkg-control
 
 sub control_index {
     my ($self, $file) = @_;
-    return $self->_fetch_index_data('control-index', 'control-index',
-        undef, 'control', $file);
+    if (my $cache = $self->{'control_index'}) {
+        return $cache->{$file}
+          if exists($cache->{$file});
+        return;
+    }
+    my $load_info = {
+        'field' => 'control_index',
+        'index_file' => 'control-index',
+        'index_owner_file' => undef,
+        'fs_root_sub' => 'control',
+        # Control files are not installed relative to the system root.
+        # Accordingly, we forbit absolute paths and symlinks..
+        'has_anchored_root_dir' => 0,
+    };
+    return $self->_fetch_index_data($load_info, $file);
 }
 
 =item sorted_control_index
@@ -218,8 +231,8 @@ sub sorted_control_index {
     my ($self) = @_;
     # control_index does all our work for us, so call it if
     # sorted_control_index has not been created yet.
-    $self->control_index('') unless exists $self->{'sorted_control-index'};
-    return @{ $self->{'sorted_control-index'} };
+    $self->control_index('') unless exists($self->{'sorted_control_index'});
+    return @{ $self->{'sorted_control_index'} };
 }
 
 =item control_index_resolved_path(PATH)

lib/Lintian/Collect/Source.pm
@@ -274,8 +274,21 @@ Needs-Info requirements for using I<orig_index>: src-orig-index
 
 sub orig_index {
     my ($self, $file) = @_;
-    return $self->_fetch_index_data('orig-index', 'src-orig-index', undef,
-        undef, $file);
+    if (my $cache = $self->{'orig_index'}) {
+        return $cache->{$file}
+          if exists($cache->{$file});
+        return;
+    }
+    my $load_info = {
+        'field' => 'orig_index',
+        'index_file' => 'src-orig-index',
+        'index_owner_file' => undef,
+        'fs_root_sub' => undef,
+        # source packages do not have anchored roots as they can be
+        # unpacked anywhere...
+        'has_anchored_root_dir' => 1,
+    };
+    return $self->_fetch_index_data($load_info, $file);
 }
 
 =item sorted_orig_index
@@ -299,8 +312,8 @@ sub sorted_orig_index {
     my ($self) = @_;
     # orig_index does all our work for us, so call it if
     # sorted_orig_index has not been created yet.
-    $self->orig_index('') unless exists $self->{'sorted_orig-index'};
-    return @{ $self->{'sorted_orig-index'} };
+    $self->orig_index('') unless exists($self->{'sorted_orig_index'});
+    return @{ $self->{'sorted_orig_index'} };
 }
 
 =item orig_index_resolved_path(PATH)
@@ -627,7 +640,21 @@ Needs-Info requirements for using I<index>: unpacked
 
 sub index {
     my ($self, $file) = @_;
-    return $self->_fetch_index_data('index', 'index', undef, 'unpacked',$file);
+    if (my $cache = $self->{'index'}) {
+        return $cache->{$file}
+          if exists($cache->{$file});
+        return;
+    }
+    my $load_info = {
+        'field' => 'index',
+        'index_file' => 'index',
+        'index_owner_file' => undef,
+        'fs_root_sub' => 'unpacked',
+        # source packages do not have anchored roots as they can be
+        # unpacked anywhere...
+        'has_anchored_root_dir' => 0,
+    };
+    return $self->_fetch_index_data($load_info, $file);
 }
 
 =item is_non_free

commit d0e04cb53a18c358f87c0ed33dd6cb894144cc48

Correct "as_anchored_root_dir" -> "has_anchored_root_dir" typo introduced in e0c833b3.

lib/Lintian/Collect/Package.pm
@@ -426,7 +426,7 @@ sub _fetch_index_data {
         '_collect' => $self,
         '_collect_path_sub' => $load_info->{'fs_root_sub'},
         '_collect_file_info_sub' => $load_info->{'file_info_sub'},
-        'has_anchored_root_dir' => $load_info->{'as_anchored_root_dir'},
+        'has_anchored_root_dir' => $load_info->{'has_anchored_root_dir'},
     );
 
     if ($indexown) {