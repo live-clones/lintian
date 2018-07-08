f482630a6b5fd93fd7fd1fd9687e31e3f5e6360e
Chris Lamb <lamby@debian.org>
2018-07-08 19:38:43 +0200

    Support parsing the filename component of tar -tvf output when delimited by multiple spaces too.  (Closes: #903307)

--- lib/Lintian/Collect/Package.pm
+++ lib/Lintian/Collect/Package.pm
@@ -445,7 +445,7 @@ sub _fetch_index_data {
         # "fake" dates.
         ($perm,$ownership,$size,$date,$time,$name)
           = $line
-          =~ /^(.{10}) (.*?) (\d+) ([-\d]{10}) ([:\d]{5,8}(?:.\d+)?) (.*)$/;
+          =~ /^(.{10}) (.*?) (\d+) ([-\d]{10}) ([:\d]{5,8}(?:.\d+)?)[ ]+(.*)$/;
         croak "cannot parse tar output from $index: $line"
           unless defined $perm;
         $ownership =~ s/\s+$//;
