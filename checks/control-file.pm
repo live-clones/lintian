933cb2e1c4e807cc8737589fc2ed6fab0bd844e4
Chris Lamb <lamby@debian.org>
2018-09-06 09:13:38 +0100

    Use get_changes_processable; it won't blow up if we don't have one.

--- checks/control-file.pm
+++ checks/control-file.pm
@@ -452,12 +452,12 @@ sub run {
           unless $info->source_field('xs-go-import-path', '');
     }
 
-    foreach my $c ($group->get_processables('changes')) {
-        tag 'source-only-upload-to-non-free-without-autobuild'
-          if $c->info->field('architecture', 'source')
-          and $info->is_non_free
-          and $info->source_field('xs-autobuild', 'no') eq 'no';
-    }
+    my $changes = $group->get_changes_processable;
+    tag 'source-only-upload-to-non-free-without-autobuild'
+      if defined($changes)
+      and $changes->info->field('architecture', 'source')
+      and $info->is_non_free
+      and $info->source_field('xs-autobuild', 'no') eq 'no';
 
     return;
 }
