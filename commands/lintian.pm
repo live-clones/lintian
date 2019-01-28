99a8b4fdc4f6c0990338c0ca7c9e0253e435e37d
Chris Lamb <lamby@debian.org>
2019-01-28 23:36:01 +0100

    commands/lintian.pm: Perltidy
    Gbp-Dch: ignore

--- commands/lintian.pm
+++ commands/lintian.pm
@@ -1470,7 +1470,7 @@ sub configure_output {
     if ($opt{'debug'}) {
         $opt{'verbose'} = 1;
         $ENV{'LINTIAN_DEBUG'} = $opt{'debug'};
-        $SIG{ __DIE__ } = sub { Carp::confess( @_ ) };
+        $SIG{__DIE__} = sub { Carp::confess(@_) };
         if ($opt{'debug'} > 2) {
             eval {
                 require Devel::Size;
