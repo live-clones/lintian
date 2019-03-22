ee239cdb4f1fe9dad0df4be22a96a862a04725ca
Felix Lechner <felix.lechner@lease-up.com>
2019-03-21 17:36:38 -0700

    In check menus, split file lists without generating extra spaces.
    Commit f3ca0a90 causes a test to fail, but the extra tags generated do
    not name any files. This is presumably because the split operator may
    work differently than intended.
    
    According to https://perldoc.perl.org/functions/split.html, split
    expects regular expressions as the split operator, except in special
    cases. One such case is a string containing a single space, which may
    be of use here. (The logical alternative /\s+/ did not work, but I did
    not try together with 'next unless length $file'.) The special string
    with one space strips any leading spaces first and therefore prevents
    any strings from being generated, which may cause the issue here.
    
    Converts the separator expression to a string (but not a regular
    expression) containing a single string. Also removes a condition for
    empty strings further below that is no longer needed.
    
    Here is the test failure that shows tags without file names, which was
    probably not intended:
    
    Failed test 'Lintian tags match for legacy-binary'
      at /lcl/lechner/lintian/git/lib/Test/Lintian/Run.pm line 385.
    Tags do not match
    --- debian/test-out/tags/checks/menus/legacy-binary/tags.specified.calibrated.universal
    +++ debian/test-out/tags/checks/menus/legacy-binary/tags.actual.parsed.sorted.universal
    +binary (binary): doc-base-file-references-wrong-path binary:39
    +binary (binary): doc-base-file-references-wrong-path binary:31
    +binary (binary): doc-base-file-references-wrong-path binary:25
    +binary (binary): doc-base-file-references-wrong-path binary:25
    Looks like you failed 1 test of 1.
    debian/test-out/tags/checks/menus/legacy-binary/generic.t ................................ Dubious, test returned 1 (wstat 256, 0x100)
    Failed 1/1 subtests
    
    Gbp-Dch: ignore

--- checks/menus.pm
+++ checks/menus.pm
@@ -345,7 +345,7 @@ sub check_doc_base_field {
     # classes since otherwise we'd need to deal with wildcards inside
     # character classes and aren't there yet.
     if ($field eq 'index' or $field eq 'files') {
-        my @files = map { split('\s+', $_) } @$vals;
+        my @files = map { split(' ', $_) } @$vals;
 
         if ($field eq 'index' && @files > 1) {
             tag 'doc-base-index-references-multiple-files', "$dbfile:$line";
@@ -356,7 +356,6 @@ sub check_doc_base_field {
             tag 'doc-base-file-references-wrong-path', "$dbfile:$line", $file;
         }
         for my $file (@files) {
-            next if $file eq '';
             my $realfile = delink($file, $all_links);
             # openoffice.org-dev-doc has thousands of files listed so try to
             # use the hash if possible.
