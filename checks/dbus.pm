live-clones/lintian

commit 59644f1a9647e14964cef77e2b8f3c5a59e0bfcf

Consider the preferred /usr/share/dbus-1/{system,session}.d directories (supported since dbus 1.9.18) when checking services. (MR: !187)
Signed-off-by: Chris Lamb <lamby@debian.org>

checks/dbus.pm
@@ -37,6 +37,10 @@ sub run {
                 push @files, $dir->children;
             }
         }
+        if (my $dir
+            = $info->index_resolved_path("usr/share/dbus-1/${dirname}.d")) {
+            push @files, $dir->children;
+        }
     }
 
     foreach my $file (@files) {

commit 7c539e08b642f6e891d2333c6dad367a713917d3

Correct $dirname typo and suffix/prefix logic error introduced in 59644f1a9.
Gbp-Dch: ignore

checks/dbus.pm
@@ -31,15 +31,15 @@ sub run {
     my ($pkg, $type, $info) = @_;
 
     my @files;
-    foreach my $prefix (qw(etc/dbus-1 usr/share/dbus-1)) {
-        foreach my $suffix (qw(session system)) {
+    foreach my $suffix (qw(session system)) {
+        if (my $dir
+            = $info->index_resolved_path("usr/share/dbus-1/${suffix}.d")) {
+            push @files, $dir->children;
+        }
+        foreach my $prefix (qw(etc/dbus-1 usr/share/dbus-1)) {
             if (my $dir = $info->index_resolved_path("${prefix}/${suffix}.d")){
                 push @files, $dir->children;
             }
-        }
-        if (my $dir
-            = $info->index_resolved_path("usr/share/dbus-1/${dirname}.d")) {
-            push @files, $dir->children;
         }
     }
 