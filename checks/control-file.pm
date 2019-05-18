live-clones/lintian

commit 2e83812c19e1466bc05e65bb3fa52b33e33305e8

Check for sources in the "main" section with only binaries in the contrib section. (Closes: #928126)
Signed-off-by: Chris Lamb <lamby@debian.org>

checks/control-file.pm
@@ -278,6 +278,7 @@ sub run {
         tag 'no-section-field-for-source';
     }
     my @descriptions;
+    my $seen_main, $seen_contrib;
     foreach my $bin (@package_names) {
 
         # Accumulate the description.
@@ -295,6 +296,7 @@ sub run {
 
         # Check mismatches in archive area.
         $bin_area = $info->binary_field($bin, 'section');
+        $seen_main = 1 if not $bin_area and $area eq 'main';
         next unless $area && $bin_area;
 
         if ($bin_area =~ m%^([^/]+)/%) {
@@ -302,12 +304,19 @@ sub run {
         } else {
             $bin_area = 'main';
         }
+        $seen_main = 1 if $bin_area eq 'main';
+        $seen_contrib = 1 if $bin_area eq 'contrib';
         next
           if $area eq $bin_area
           or ($area eq 'main' and $bin_area eq 'contrib');
 
         tag 'section-area-mismatch', 'Package', $bin;
     }
+
+    tag 'section-area-mismatch'
+      if $seen_contrib
+      and not $seen_main
+      and $area eq 'main';
 
     # Check for duplicate descriptions.
     my (%seen_short, %seen_long);

checks/control-file.desc
@@ -103,7 +103,8 @@ Info: The <tt>debian/control</tt> file places the indicated binary package
  package or other binary packages built from the same source package. The
  source package and any binary packages it builds must be in the same
  area of the archive, with the single exception that source packages in
- main may also build binary packages in contrib.
+ main may also build binary packages in contrib if they build binary
+ packages in main.
 
 Tag: xs-vcs-field-in-debian-control
 Severity: wishlist

commit c6f3d71dd6230e05a33868a86b1aac7d8979b4d9

Correct $area initialisation issue introduced in 2e83812c19e146.
Gbp-Dch: ignore

checks/control-file.pm
@@ -296,7 +296,7 @@ sub run {
 
         # Check mismatches in archive area.
         $bin_area = $info->binary_field($bin, 'section');
-        $seen_main = 1 if not $bin_area and $area eq 'main';
+        $seen_main = 1 if not $bin_area and ($area // '') eq 'main';
         next unless $area && $bin_area;
 
         if ($bin_area =~ m%^([^/]+)/%) {