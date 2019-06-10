501c1b8b8bc0f8f3a064212a2cf92a5b491ae482
Chris Lamb <lamby@debian.org>
2019-06-10 21:24:25 +0100

    Exclude "X Bitmap Graphic" (.xbm) files from being flagged as extra license files. (Closes: #930211)

--- checks/files.pm
+++ checks/files.pm
@@ -1346,7 +1346,7 @@ sub run {
             and not $fname =~ m{ \. (?:
                   # Common "non-license" file extensions...
                    el|[ch]|cc|p[ylmc]|[hu]i|p_hi|html|php|rb|xpm
-                     |png|jpe?g|gif|svg|dtd|mk|lisp|yml|rs|ogg
+                     |png|jpe?g|gif|svg|dtd|mk|lisp|yml|rs|ogg|xbm
                ) \Z}xsm
             and not $fname=~ m,^usr/share/zope/Products/.*\.(?:dtml|pt|cpt)$,
             and not $fname =~ m,/under\S+License\.docbook$,
