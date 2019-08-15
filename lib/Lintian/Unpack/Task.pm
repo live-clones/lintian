81ff8a3e6ee23c176b3080013e6006aab343c686
Felix Lechner <felix.lechner@lease-up.com>
2019-08-15 14:37:27 -0700

    Eliminate two remaining calls to IO::Async::Loop->really_new, which are rarely a good idea.
    For some time, I received guidance to sometimes use ->really_new
    instead of the plain ->new. Using the same loop led to confusion in
    nested use as described in commit d4a90c21.
    
    It seems the problem was resolved in IO::Async version 0.73 and
    Lintian also currently undefines the loop singleton in nested usage
    for backports.  The calls to ->really_new are no longer necessary.
    This removes them.
    
    Gbp-Dch: ignore

--- lib/Lintian/Unpack/Task.pm
+++ lib/Lintian/Unpack/Task.pm
@@ -97,7 +97,7 @@ sub run {
     #     $ENV{'PERL5OPT'} = "${p5opt} ${coverage_arg}";
     # }
 
-    my $loop = IO::Async::Loop->really_new;
+    my $loop = IO::Async::Loop->new;
     my $future = $loop->new_future;
     my $process = IO::Async::Process->new(
 
