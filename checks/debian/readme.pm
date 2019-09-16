e88823458b47babd35935bf126e03e68bb744e4e
Felix Lechner <felix.lechner@lease-up.com>
2019-09-16 16:32:05 -0700

    Move check debian-readme to 'debian/readme'.
    This is one of several upcoming changes that will help break down and
    organize the massive and monolithic check scripts that accumulated
    over the decades.

--- checks/debian/readme.pm
+++ checks/debian/readme.pm
@@ -1,4 +1,4 @@
-# debian-readme -- lintian check script -*- perl -*-
+# debian/readme -- lintian check script -*- perl -*-
 
 # Copyright (C) 1998 Richard Braakman
 #
@@ -18,7 +18,8 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::debian_readme;
+package Lintian::debian::readme;
+
 use strict;
 use warnings;
 use autodie;
