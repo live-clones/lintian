67bbbdae84b1f045bf75450f2912cb4d369109a3
Felix Lechner <felix.lechner@lease-up.com>
2019-09-16 16:33:39 -0700

    Move check debian-source-dir to 'debian/source-dir'.
    This is one of several upcoming changes that will help break down and
    organize the massive and monolithic check scripts that accumulated
    over the decades.

--- checks/debian/source-dir.pm
+++ checks/debian/source-dir.pm
@@ -18,7 +18,8 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::debian_source_dir;
+package Lintian::debian::source_dir;
+
 use strict;
 use warnings;
 use autodie;
