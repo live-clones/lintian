0aca51f794b6cfdb3eb04e4a2af15c08122b0f81
Felix Lechner <felix.lechner@lease-up.com>
2019-09-16 14:41:57 -0700

    Move check copyright to 'debian/copyright'.
    This commit is part of a series that makes it possible to sort checks
    in subfolders and identify them by path.
    
    Moves the check copyright to debian/copyright.

--- checks/debian/copyright.pm
+++ checks/debian/copyright.pm
@@ -19,7 +19,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::copyright;
+package Lintian::debian::copyright;
 
 use strict;
 use warnings;
