beaaf79ff265cf859d7cf4d8dde8b440e001fc89
Felix Lechner <felix.lechner@lease-up.com>
2019-10-31 16:12:17 -0700

    From Lintian::Lab, remove instance method get_package, which was transferred to Processable::Group.
    This commit is part of an ongoing effort to simplify the main data
    flow in Lintian. One goal is to place code in the proper class, which
    was advanced here.
    
    Gbp-Dch: ignore

--- lib/Lintian/Lab.pm
+++ lib/Lintian/Lab.pm
@@ -31,17 +31,6 @@ use Cwd();
 use File::Temp qw(tempdir);
 use Path::Tiny;
 
-# A private table of supported types.
-my %SUPPORTED_TYPES = (
-    'binary'  => 1,
-    'buildinfo' => 1,
-    'changes' => 1,
-    'source'  => 1,
-    'udeb'    => 1,
-);
-
-use Lintian::Lab::Entry;
-
 use constant EMPTY => q{};
 
 # must be absolute; frontend/lintian depends on it
@@ -82,63 +71,6 @@ well as providing access to the entries.
 =head1 INSTANCE METHODS
 
 =over 4
-
-=item get_package (PROC)
-
-Fetches an existing package from the lab.
-
-The first argument must be a L<processable|Lintian::Processable>.
-
-=cut
-
-sub get_package {
-    my ($self, $proc) = @_;
-
-    return $proc
-      if $proc->isa('Lintian::Lab::Entry') && $proc->from_lab($self);
-
-    my $pkg_type = $proc->pkg_type;
-
-    # get_package only works with "real" types (and not views).
-    croak "Not a supported type ($pkg_type)"
-      unless exists $SUPPORTED_TYPES{$pkg_type};
-
-    my $dir = $self->_pool_path($proc->pkg_src,$pkg_type,$proc->pkg_name,
-        $proc->pkg_version,$proc->pkg_arch);
-
-    my $entry = Lintian::Lab::Entry->_new_from_proc($proc, $self, $dir);
-    return $entry;
-}
-
-# Given the package meta data (src_name, type, name, version, arch) return the
-# path to it in the Lab.  The path returned will be absolute.
-sub _pool_path {
-    my ($self, $pkg_src, $pkg_type, $pkg_name, $pkg_version, $pkg_arch) = @_;
-
-    my $dir = $self->basedir;
-    my $p;
-
-    # If it is at least 4 characters and starts with "lib", use "libX"
-    # as prefix
-    if ($pkg_src =~ m/^lib./o) {
-        $p = substr $pkg_src, 0, 4;
-    } else {
-        $p = substr $pkg_src, 0, 1;
-    }
-
-    $p  = "$p/$pkg_src/${pkg_name}_${pkg_version}";
-    $p .= "_${pkg_arch}" unless $pkg_type eq 'source';
-    $p .= "_${pkg_type}";
-
-    # Turn spaces into dashes - spaces do appear in architectures
-    # (i.e. for changes files).
-    $p =~ s/\s/-/go;
-
-    # Also replace ":" with "_" as : is usually used for path separator
-    $p =~ s/:/_/go;
-
-    return "$dir/pool/$p";
-}
 
 =item DEMOLISH
 
