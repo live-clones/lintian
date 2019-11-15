6686b95b188a0db6c2ca8c497bd69be9c3bf6ea6
Felix Lechner <felix.lechner@lease-up.com>
2019-11-14 19:25:32 -0800

    Provide a new class for issued tags that concatenate their arguments with spaces in between.
    This class instantiates tags that have been found. That is in contrast
    to Tag::Info, which is a class of tag prototypes.  Tags represented by
    this class concatenate their arguments with a space in between, as all
    tags do presently.
    
    At some point, other classes could accept higher order arguments and
    convert them to strings inside the tag object. One benefit would be
    consistent formatting, but that alone may not be worth the complexity
    of having additional classes.
    
    Gbp-Dch: ignore

---
+++ lib/Lintian/Tag/Standard.pm
@@ -0,0 +1,57 @@
+# Copyright © 2019 Felix Lechner
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, you can find it on the World Wide
+# Web at http://www.gnu.org/copyleft/gpl.html, or write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+package Lintian::Tag::Standard;
+
+use strict;
+use warnings;
+
+use Moo;
+use namespace::clean;
+
+with 'Lintian::Tag';
+
+=head1 NAME
+
+Lintian::Tag::Standard - standard tag with arguments concatenated by space
+
+=head1 SYNOPSIS
+
+    use Lintian::Tag::Standard;
+
+=head1 DESCRIPTION
+
+Provides a standard tag whose arguments are concatenated by a space.
+
+=head1 INSTANCE METHODS
+
+=over 4
+
+=back
+
+=cut
+
+1;
+
+__END__
+
+# Local Variables:
+# indent-tabs-mode: nil
+# cperl-indent-level: 4
+# End:
+# vim: syntax=perl sw=4 sts=4 sr et
