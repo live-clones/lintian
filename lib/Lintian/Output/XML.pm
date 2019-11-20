4ee8d857e6981e71ec13c6c390781e08090be341
Felix Lechner <felix.lechner@lease-up.com>
2019-11-20 05:12:58 -0800

    Devolve most methods helping issue tags into Output::XML from Lintian::Output.
    This is part of an effort to split tag output from the convoluted
    object logic in Lintian::Output.
    
    Gbp-Dch: ignore

--- lib/Lintian/Output/XML.pm
+++ lib/Lintian/Output/XML.pm
@@ -23,6 +23,10 @@ use warnings;
 
 use HTML::Entities;
 
+use constant EMPTY => q{};
+use constant SPACE => q{ };
+use constant NEWLINE => qq{\n};
+
 use Moo;
 use namespace::clean;
 
@@ -44,6 +48,46 @@ Provides XML tag output.
 
 =over 4
 
+=item issue_tags
+
+Print all tags passed in array. A separate arguments with processables
+is necessary to report in case no tags were found.
+
+=cut
+
+sub issue_tags {
+    my ($self, $pending, $processables) = @_;
+
+    return
+      unless $pending && $processables;
+
+    my %taglist;
+
+    for my $tag (@{$pending}) {
+        $taglist{$tag->processable} //= [];
+        push(@{$taglist{$tag->processable}}, $tag);
+    }
+
+    for my $processable (@{$processables}) {
+
+        my @attrs = (
+            [type         => $processable->type],
+            [name         => $processable->name],
+            [architecture => $processable->architecture],
+            [version      => $processable->version]);
+
+        my $preamble = $self->_open_xml_tag('package', \@attrs, 0);
+        print { $self->stdout } $preamble, NEWLINE;
+
+        my @sorted = @{$taglist{$processable} // []};
+        $self->print_tag($_) for @sorted;
+
+        print { $self->stdout } "</package>\n";
+    }
+
+    return;
+}
+
 =item print_tag
 
 =cut
@@ -55,7 +99,8 @@ sub print_tag {
     my $information = $tag->extra;
     my $override = $tag->override;
 
-    $self->issued_tag($tag_info->tag);
+    $self->issuedtags->{$tag_info->tag}++;
+
     my $flags = ($tag_info->experimental ? 'experimental' : '');
     my $comment;
     if ($override) {
@@ -79,29 +124,18 @@ sub print_tag {
     return;
 }
 
-=item print_start_pkg
+=item C<_quote_print($string)>
+
+Called to quote a string.  By default it will replace all
+non-printables with "?".  Sub-classes can override it if
+they allow non-ascii printables etc.
 
 =cut
 
-sub print_start_pkg {
-    my ($self, $processable) = @_;
-    my @attrs = (
-        [type         => $processable->type],
-        [name         => $processable->name],
-        [architecture => $processable->architecture],
-        [version      => $processable->version]);
-    print { $self->stdout } $self->_open_xml_tag('package', \@attrs, 0), "\n";
-    return;
-}
-
-=item print_end_pkg
-
-=cut
-
-sub print_end_pkg {
-    my ($self) = @_;
-    print { $self->stdout } "</package>\n";
-    return;
+sub _quote_print {
+    my ($self, $string) = @_;
+    $string =~ s/[^[:print:]]/?/go;
+    return $string;
 }
 
 sub _delimiter {
