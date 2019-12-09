6a07df2e4c8c9f6b2b12ef39d22b2a68f5c5a326
Felix Lechner <felix.lechner@lease-up.com>
2019-12-09 09:55:12 -0800

    Add new check for unsafe mailcap entries. (Closes: #33486)
    This check parses mailcap files line by line. It emits a tag if the
    placeholder (%s) is used with quotes.
    
    As detailed in these references, quoting should be left to the program
    that uses the files. Otherwise, it is considered unsafe.
    
        Bug#33486
        Bug#90483
        Bug#745141
        http://bugs.debian.org/745141#17
        https://lists.debian.org/debian-user/2005/04/msg01185.html
        https://tools.ietf.org/rfc/rfc1524.txt
    
    The bug is from 1999. The mailcap specification is from 1993. The
    quoting question was presumably considered with some care.
    
    Only a few programs still ship mailcap files.

---
+++ checks/mailcap.pm
@@ -0,0 +1,103 @@
+# mailcap -- lintian check script -*- perl -*-
+
+# Copyright © 2019 Felix Lechner
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, you can find it on the World Wide
+# Web at http://www.gnu.org/copyleft/gpl.html, or write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+package Lintian::mailcap;
+
+use strict;
+use warnings;
+
+use List::MoreUtils qw(uniq);
+use Text::Balanced qw(extract_delimited extract_multiple);
+
+use constant EMPTY => q{};
+use constant SPACE => q{ };
+use constant COLON => q{:};
+
+use Moo;
+use namespace::clean;
+
+with 'Lintian::Check';
+
+sub files {
+    my ($self, $file) = @_;
+
+    return
+      unless $file->name =~ m{^usr/lib/mime/packages/};
+
+    return
+      unless $file->is_file && $file->is_open_ok;
+
+    my $fd = $file->open;
+
+    my @continuation;
+
+    while (<$fd>) {
+
+        unless (@continuation) {
+            # skip blank lines
+            next
+              if /^\s*$/;
+
+            # skip comments
+            next
+              if /^\s*#/;
+        }
+
+        # continuation line
+        if (s/\\$//) {
+            push(@continuation, {string => $_, position => $.});
+            next;
+        }
+
+        push(@continuation, {string => $_, position => $.});
+
+        my $line = EMPTY;
+        $line .= $_->{string}for @continuation;
+
+        my $position = $continuation[0]->{position};
+
+        my @quoted
+          = extract_multiple($line,
+            [sub { extract_delimited($_[0], q{"'}, '[^\'"]*') }],
+            undef, 1);
+
+        my @placeholders = uniq grep { /\%s/ } @quoted;
+
+        $self->tag(
+            'quoted-placeholder-in-mailcap-entry',
+            $file->name . COLON . $position,
+            @placeholders
+        )if @placeholders;
+
+        @continuation = ();
+    }
+
+    close($fd);
+
+    return;
+}
+
+1;
+
+# Local Variables:
+# indent-tabs-mode: nil
+# cperl-indent-level: 4
+# End:
+# vim: syntax=perl sw=4 sts=4 sr et
