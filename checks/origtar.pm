a41e0a308d3c6594b06ac77f54eb29df6fa26ee8
Felix Lechner <felix.lechner@lease-up.com>
2019-12-14 06:28:47 -0800

    Add new check for orig tarball. (Closes: #471537)
    This commit was prompted by Bug#471537. The patch submitted there no
    longer worked with the current source tree, but some relevant ideas
    were extracted. Lintian now issues a warning for empty orig sources.
    
    The check for repackaged source was transfered to debian/copyright. It
    now makes sure the version string reflects a repackaging.
    
    The idea to check for the rules target get-origtargz was not adopted,
    as uscan is now widely used for automated repacking. This commit
    series does not check the contents of watch files.

---
+++ checks/origtar.pm
@@ -0,0 +1,54 @@
+# origtar -- lintian check script -*- perl -*-
+#
+# Copyright © 2008 Bernhard R. Link
+# Copyright © 2019 Felix Lechner
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, you can find it on the World Wide
+# Web at http://www.gnu.org/copyleft/gpl.html, or write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+package Lintian::origtar;
+
+use warnings;
+use strict;
+
+use Moo;
+use namespace::clean;
+
+with 'Lintian::Check';
+
+sub source {
+    my ($self) = @_;
+
+    my $processable = $self->processable;
+
+    return
+      if $processable->native;
+
+    my @origfiles = $processable->sorted_orig_index;
+
+    $self->tag('empty-upstream-sources')
+      unless @origfiles;
+
+    return;
+}
+
+1;
+
+# Local Variables:
+# indent-tabs-mode: nil
+# cperl-indent-level: 4
+# End:
+# vim: syntax=perl sw=4 sts=4 sr et
