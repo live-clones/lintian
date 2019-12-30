fd66c622499a175964844264f4f3a954b031a2af
Felix Lechner <felix.lechner@lease-up.com>
2019-12-30 07:32:11 -0800

    Let check x11 stand on its own; simplify it.
    Check is being split from 'files'. Code was simplified. Location
    moved.
    
    Gbp-Dch: ignore

--- checks/x11.pm
+++ checks/x11.pm
@@ -1,4 +1,4 @@
-# files/x11 -- lintian check script -*- perl -*-
+# x11 -- lintian check script -*- perl -*-
 
 # Copyright (C) 1998 Christian Schwarz and Richard Braakman
 #
@@ -18,44 +18,50 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::files::x11;
+package Lintian::x11;
 
 use strict;
 use warnings;
 use autodie;
+
+use List::MoreUtils qw(any none);
 
 use Moo;
 use namespace::clean;
 
 with 'Lintian::Check';
 
-has x11_font_dirs => (is => 'rwp', default => sub { {} });
+has fontdirs => (is => 'rw', default => sub { {} });
 
 sub files {
     my ($self, $file) = @_;
 
     # links to FHS locations are allowed
-    if ($file->name =~ m,^usr/X11R6/, and not $file->is_symlink) {
-        $self->tag('package-installs-file-to-usr-x11r6', $file->name);
-    }
+    $self->tag('package-installs-file-to-usr-x11r6', $file->name)
+      if $file->name =~ m,^usr/X11R6/, && !$file->is_symlink;
+
+    return
+      if $file->is_dir;
 
     # /usr/share/fonts/X11
-    if ($file->name =~ m,^usr/share/fonts/X11/([^/]+)/\S+,) {
+    my ($subdir) = ($file->name =~ m,^usr/share/fonts/X11/([^/]+)/\S+,);
+    if (defined $subdir) {
 
-        my $dir = $1;
-        if ($dir =~ /^(?:PEX|CID|Speedo|cyrillic)$/) {
+        $self->fontdirs->{$subdir}++
+          if any { $subdir eq $_ } qw(100dpi 75dpi misc);
+
+        if (any { $subdir eq $_ } qw(PEX CID Speedo cyrillic)) {
             $self->tag('file-in-discouraged-x11-font-directory', $file->name);
 
-        } elsif ($dir !~ /^(?:100dpi|75dpi|misc|Type1|encodings|util)$/) {
+        } elsif (
+            none { $subdir eq $_ }
+            qw(100dpi 75dpi misc Type1 encodings util)
+        ) {
             $self->tag('file-in-unknown-x11-font-directory', $file->name);
 
         } elsif ($file->basename eq 'encodings.dir'
             or $file->basename =~ m{fonts\.(dir|scale|alias)}) {
             $self->tag('package-contains-compiled-font-file', $file->name);
-        }
-
-        if ($dir =~ /^(?:100dpi|75dpi|misc)$/) {
-            $self->x11_font_dirs->{$dir}++;
         }
     }
 
@@ -65,17 +71,15 @@ sub files {
 sub breakdown {
     my ($self) = @_;
 
-    # X11 bitmapped font directories under /usr/share/fonts/X11 in which we've
-    # seen files.
-    my %x11_font_dirs = %{$self->x11_font_dirs};
+    # X11 font directories with files
+    my %fontdirs = %{$self->fontdirs};
 
     # check for multiple DPIs in the same X11 bitmap font package.
-    if ($x11_font_dirs{'100dpi'} and $x11_font_dirs{'75dpi'}) {
-        $self->tag('package-contains-multiple-dpi-fonts');
-    }
-    if ($x11_font_dirs{misc} and keys(%x11_font_dirs) > 1) {
-        $self->tag('package-mixes-misc-and-dpi-fonts');
-    }
+    $self->tag('package-contains-multiple-dpi-fonts')
+      if $fontdirs{'100dpi'} && $fontdirs{'75dpi'};
+
+    $self->tag('package-mixes-misc-and-dpi-fonts')
+      if $fontdirs{misc} && keys %fontdirs > 1;
 
     return;
 }
