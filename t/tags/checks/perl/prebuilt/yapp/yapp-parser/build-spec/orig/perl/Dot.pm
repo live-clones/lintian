0837bbdb305056c6af3ad89cb0f8e025745381ea
Felix Lechner <felix.lechner@lease-up.com>
2020-01-22 14:03:19 -0800

    Provide test for new tag source-contains-prebuilt-yapp-parser.
    Test file data was taken from lib/Graph/Reader/Dot.pm in
    libgraph-readwrite-perl-2.09 as described in the message
    https://lists.debian.org/debian-perl/2018/07/msg00026.html.
    
    Prompted by #921080.
    
    Gbp-Dch: ignore

---
+++ t/tags/checks/perl/prebuilt/yapp/yapp-parser/build-spec/orig/perl/Dot.pm
@@ -0,0 +1,14 @@
+####################################################################
+#
+#    This file was generated using Parse::Yapp version 1.02.
+#
+#        Don't edit this file, use source file instead.
+#
+#             ANY CHANGE MADE HERE WILL BE LOST !
+#
+####################################################################
+
+Taken from lib/Graph/Reader/Dot.pm in libgraph-readwrite-perl-2.09
+on 1/22/20
+
+Felix Lechner
