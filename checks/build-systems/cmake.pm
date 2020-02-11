6c9f12ddfb55908a3554c54691c589ef3428f5b5
Felix Lechner <felix.lechner@lease-up.com>
2020-02-11 07:36:20 -0800

    Move check files/cmake to build-systems/cmake.
    Gbp-Dch: ignore

--- checks/build-systems/cmake.pm
+++ checks/build-systems/cmake.pm
@@ -1,4 +1,4 @@
-# files/cmake -- lintian check script -*- perl -*-
+# build-systems/cmake -- lintian check script -*- perl -*-
 
 # Copyright (C) 1998 Christian Schwarz and Richard Braakman
 #
@@ -18,7 +18,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::files::cmake;
+package Lintian::build_systems::cmake;
 
 use strict;
 use warnings;
