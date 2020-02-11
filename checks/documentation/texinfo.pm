8f314fd8938cb38be9aac53efbc52fd5fb266917
Felix Lechner <felix.lechner@lease-up.com>
2020-02-11 07:47:00 -0800

    Move check infofiles to documentation/texinfo.
    Gbp-Dch: ignore

--- checks/documentation/texinfo.pm
+++ checks/documentation/texinfo.pm
@@ -1,4 +1,4 @@
-# infofiles -- lintian check script -*- perl -*-
+# documentation/texinfo -- lintian check script -*- perl -*-
 
 # Copyright (C) 1998 Christian Schwarz
 # Copyright (C) 2001 Josip Rodin
@@ -19,7 +19,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::infofiles;
+package Lintian::documentation::texinfo;
 
 use strict;
 use warnings;
