a6affa2c724ae2a521c7cc9b4c0cc38c7e621163
Felix Lechner <felix.lechner@lease-up.com>
2020-02-11 07:16:49 -0800

    Move check files/perl to languages/perl.
    Gbp-Dch: ignore

--- checks/languages/perl.pm
+++ checks/languages/perl.pm
@@ -1,4 +1,4 @@
-# files/perl -- lintian check script -*- perl -*-
+# languages/perl -- lintian check script -*- perl -*-
 
 # Copyright (C) 1998 Christian Schwarz and Richard Braakman
 #
@@ -18,7 +18,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::files::perl;
+package Lintian::languages::perl;
 
 use strict;
 use warnings;
