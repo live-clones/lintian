8f8c83588742926a78964b156feea685bc085c47
Felix Lechner <felix.lechner@lease-up.com>
2020-02-11 08:01:33 -0800

    Move check files/fonts to fonts.
    Gbp-Dch: ignore

--- checks/fonts.pm
+++ checks/fonts.pm
@@ -1,4 +1,4 @@
-# files/fonts -- lintian check script -*- perl -*-
+# fonts -- lintian check script -*- perl -*-
 
 # Copyright (C) 1998 Christian Schwarz and Richard Braakman
 #
@@ -18,7 +18,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::files::fonts;
+package Lintian::fonts;
 
 use strict;
 use warnings;
