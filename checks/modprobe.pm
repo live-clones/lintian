216dcc4237d6d4eeaf8bbcadf3e3ec210d15fec0
Felix Lechner <felix.lechner@lease-up.com>
2020-02-11 15:04:02 -0800

    Move check files/modprobe to modprobe.
    Gbp-Dch: ignore

--- checks/modprobe.pm
+++ checks/modprobe.pm
@@ -1,4 +1,4 @@
-# files/modprobe -- lintian check script -*- perl -*-
+# modprobe -- lintian check script -*- perl -*-
 
 # Copyright (C) 1998 Christian Schwarz and Richard Braakman
 #
@@ -18,7 +18,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::files::modprobe;
+package Lintian::modprobe;
 
 use strict;
 use warnings;
