cf0249384c734a2971c226f8b7f82a0d48dd1c70
Felix Lechner <felix.lechner@lease-up.com>
2020-02-11 08:56:33 -0800

    Move check files/thumbnails to images/thumbnails.
    Gbp-Dch: ignore

--- checks/images/thumbnails.pm
+++ checks/images/thumbnails.pm
@@ -1,4 +1,4 @@
-# files/thumbnails -- lintian check script -*- perl -*-
+# images/thumbnails -- lintian check script -*- perl -*-
 
 # Copyright (C) 1998 Christian Schwarz and Richard Braakman
 #
@@ -18,7 +18,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::files::thumbnails;
+package Lintian::images::thumbnails;
 
 use strict;
 use warnings;
