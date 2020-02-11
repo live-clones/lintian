be676fcebd17108c49a74fa1eea3d160c8376200
Felix Lechner <felix.lechner@lease-up.com>
2020-02-11 15:08:37 -0800

    Move check files/vim to vim.
    Gbp-Dch: ignore

--- checks/vim.pm
+++ checks/vim.pm
@@ -1,4 +1,4 @@
-# files/vim -- lintian check script -*- perl -*-
+# vim -- lintian check script -*- perl -*-
 
 # Copyright (C) 1998 Christian Schwarz and Richard Braakman
 #
@@ -18,7 +18,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::files::vim;
+package Lintian::vim;
 
 use strict;
 use warnings;
