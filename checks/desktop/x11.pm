2bf5b0f7d26691f01135ed890adac267145823d4
Felix Lechner <felix.lechner@lease-up.com>
2020-02-11 10:35:59 -0800

    Move check x11 to desktop/x11.
    Gbp-Dch: ignore

--- checks/desktop/x11.pm
+++ checks/desktop/x11.pm
@@ -1,4 +1,4 @@
-# x11 -- lintian check script -*- perl -*-
+# desktop/x11 -- lintian check script -*- perl -*-
 
 # Copyright (C) 1998 Christian Schwarz and Richard Braakman
 #
@@ -18,7 +18,7 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::x11;
+package Lintian::desktop::x11;
 
 use strict;
 use warnings;
