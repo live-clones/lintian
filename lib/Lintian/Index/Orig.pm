e9971c599bf2fd83f27ec72cdc6e105a1f1d06f9
Felix Lechner <felix.lechner@lease-up.com>
2020-03-01 20:27:36 -0800

    Transfer collection/src-orig-index into Index::Orig.
    This commit is part of a series to get rid of the collection databases
    and the collection scripts. Ultimately, it will also run checks in
    parallel (which they currently do not).
    
    Gbp-Dch: ignore

--- lib/Lintian/Index/Orig.pm
+++ lib/Lintian/Index/Orig.pm
@@ -22,6 +22,24 @@ package Lintian::Index::Orig;
 
 use strict;
 use warnings;
+use autodie;
+
+use BerkeleyDB;
+use Carp;
+use Cwd();
+use IO::Async::Loop;
+use IO::Async::Process;
+use MLDBM qw(BerkeleyDB::Btree Storable);
+use Path::Tiny;
+
+use Lintian::Collect::Dispatcher qw(create_info);
+use Lintian::Util qw(get_dsc_info);
+
+use constant EMPTY => q{};
+use constant SPACE => q{ };
+use constant COLON => q{:};
+use constant SLASH => q{/};
+use constant NEWLINE => qq{\n};
 
 use Moo;
 use namespace::clean;
@@ -51,7 +69,250 @@ in the collections scripts used previously.
 
 =over 4
 
+=item collect
 
+=item create
+
+=cut
+
+sub collect {
+    my ($self, @args) = @_;
+
+    my ($pkg, $type, $dir) = @args;
+
+    # source packages can be unpacked anywhere; no anchored roots
+    $self->allow_empty(1);
+
+    $self->create(@args);
+    $self->load("$dir/src-orig-index.db");
+
+    return;
+}
+
+sub create {
+    my ($self, $pkg, $type, $dir) = @_;
+
+    my $info = create_info($pkg, $type, $dir);
+
+    my $dbpath = "$dir/src-orig-index.db";
+    unlink($dbpath)
+      if -f $dbpath;
+
+    # do nothing for native packages
+    return
+      if $info->native;
+
+    my $dsclink = "$dir/dsc";
+    my $dscpath = Cwd::realpath($dsclink);
+    die "Cannot resolve 'dsc' link for $pkg: $dsclink"
+      unless $dscpath;
+    die "The 'dsc' link for $pkg does not point to a file: $dscpath"
+      unless -e $dscpath;
+
+    # determine source and version; handles missing fields
+
+    my $dinfo = get_dsc_info($dscpath)
+      or croak $dscpath . ' is not valid dsc file';
+
+    my $name = $dinfo->{source} // EMPTY;
+    my $version = $dinfo->{version} // EMPTY;
+    my $architecture = 'source';
+
+    # it is its own source package
+    my $source = $name;
+    my $source_version = $version;
+
+    croak $dscpath . ' is missing Source field'
+      unless length $name;
+
+    #  Version handling is based on Dpkg::Version::parseversion.
+    my $noepoch = $source_version;
+    if ($noepoch =~ /:/) {
+        $noepoch =~ s/^(?:\d+):(.+)/$1/
+          or die "Bad version number '$noepoch'";
+    }
+
+    my $baserev = $source . '_' . $noepoch;
+
+    # strip debian revision
+    $noepoch =~ s/(.+)-(?:.*)$/$1/;
+    my $base = $source . '_' . $noepoch;
+
+    my @files = split(/\n/, $info->field('files') // EMPTY);
+
+    my %components;
+    for my $line (@files) {
+
+        # strip leading whitespace
+        $line =~ s/^\s*//;
+
+        next
+          unless length $line;
+
+        # get file name
+        my (undef, undef, $name) = split(/\s+/, $line);
+
+        next
+          unless length $name;
+
+        # skip if files in subdirs
+        next
+          if $name =~ m{/};
+
+        # Look for $pkg_$version.orig(-$comp)?.tar.$ext (non-native)
+        #       or $pkg_$version.tar.$ext (native)
+        #  - This deliberately does not look for the debian packaging
+        #    even when this would be a tarball.
+        if ($name
+            =~ /^(?:\Q$base\E\.orig(?:-(.*))?|\Q$baserev\E)\.tar\.(?:gz|bz2|lzma|xz)$/
+        ) {
+            $components{$name} = $1 // EMPTY;
+        }
+    }
+
+    die "Could not find any source components for $pkg"
+      unless %components;
+
+    my %all;
+    for my $tarball (sort keys %components) {
+
+        my $component = $components{$tarball};
+
+        my @tar_options= (
+            '--list', '--verbose',
+            '--utc', '--full-time',
+            '--quoting-style=c','--file'
+        );
+
+        # may not be needed; modern tar recognizes lzma and xz
+        if ($tarball =~ /\.(lzma|xz)\z/) {
+            unshift @tar_options, "--$1";
+        }
+
+        my @tar = ('tar', @tar_options, "$dir/$tarball");
+
+        my $loop = IO::Async::Loop->new;
+        my $future = $loop->new_future;
+        my $stdout;
+        my $stderr;
+
+        my $process = IO::Async::Process->new(
+            command => [@tar],
+            stdout => { into => \$stdout },
+            stderr => { into => \$stderr },
+            on_finish => sub {
+                my ($self, $exitcode) = @_;
+                my $status = ($exitcode >> 8);
+
+                path("$dir/orig-index-errors")->append($stderr // EMPTY);
+
+                if ($status) {
+                    my $message
+                      = "Non-zero status $status from dpkg-deb for control";
+                    $message .= COLON . NEWLINE . $stderr
+                      if length $stderr;
+                    $future->fail($message);
+                    return;
+                }
+
+                $future->done('Done with dpkg-deb');
+                return;
+            });
+
+        $loop->add($process);
+
+        $future->get;
+
+        my @lines = split(/\n/, $stdout);
+
+        my %single;
+        for my $line (@lines) {
+
+            my $entry = Lintian::File::Path->new;
+            $entry->init_from_tar_output($line);
+
+            $single{$entry->name} = $entry;
+        }
+
+        # remove base directory from output
+        delete $single{''}
+          if exists $single{''};
+
+        my $unwanted = EMPTY;
+        for my $name (keys %single) {
+
+            my ($candidate) = ($name =~ m,^([^/]+),);
+
+            unless (length $candidate) {
+                $unwanted = EMPTY;
+                next;
+            }
+
+            if ($candidate eq $name
+                && !$single{$name}->perm =~ m/^d/) {
+                $unwanted = EMPTY;
+                next;
+            }
+
+            unless (length $unwanted) {
+                $unwanted = $candidate;
+                next;
+            }
+
+            next
+              if $candidate eq $unwanted;
+
+            $unwanted = EMPTY;
+        }
+
+        # If there is a common prefix and it is $component, then we use that
+        # because that is where they will be extracted by unpacked.
+        unless ($unwanted eq $component) {
+
+            my %copy;
+            for my $name (keys %single) {
+
+                my $adjusted = $name;
+
+                # strip common prefix
+                $adjusted =~ s{^\Q$unwanted\E/+}{}
+                  if length $unwanted;
+
+                # add component name
+                $adjusted = $component . SLASH . $adjusted
+                  if length $component;
+
+                # change name of entry
+                $single{$name}->name($adjusted);
+
+                # store entry under new name
+                $copy{$adjusted} = $single{$name};
+            }
+
+            %single = %copy;
+        }
+
+        $all{$_} = $single{$_} for keys %single;
+    }
+
+    # treat hard links like regular files
+    for my $name (keys %all) {
+        my $perm = $all{$name}->perm;
+        $perm =~ s/^h/-/;
+        $all{$name}->perm($perm);
+    }
+
+    tie my %h, 'MLDBM',
+      -Filename => $dbpath,
+      -Flags    => DB_CREATE
+      or die "Cannot open file $dbpath: $! $BerkeleyDB::Error\n";
+
+    $h{$_} = $all{$_} for keys %all;
+
+    untie %h;
+
+    return;
+}
 
 =back
 
