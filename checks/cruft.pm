live-clones/lintian

commit e0f6555c929150b0eab05d0baff6e274ccd836a3

Require Perl 5.20 and use pragma utf8 throughout.
A higher Perl version was required for better unicode handling. This was
done after consultation with perl-help.

Even oo-stable has Perl 5.20, so it seemed a reasonable choice.

checks/cruft.pm
@@ -25,10 +25,10 @@
 
 package Lintian::cruft;
 
-use strict;
+use v5.20;
 use warnings;
+use utf8;
 use autodie;
-use v5.10;
 
 use Carp qw(croak);
 use File::Basename qw(basename);

commit 5ac8325c28d7a823362b7845a11c00d68eb222e2

Look for trailing whitespace with Unicode in mind; issue new tag for text files without newlines at end.
Commit e0f6555c caused a regression in Lintian's own changelog:

    P: lintian source: file-contains-trailing-whitespace debian/changelog (line 17736)
    P: lintian source: file-contains-trailing-whitespace debian/changelog (line 18043)

Fixes string processing when looking for trailing whitespace under
Unicode.

From now on, the whitespace check ignores files that use a national
encoding.  Converts one test case to UTF-8. The additonal tags were
more valuable than the sole tag about the national encoding.

There will be additional regressions from the cruft check. It will be
split into properly working Unicode parts in the near future.

This commit is on the verbose side. It will be condensed when the
check is split.

Gbp-Dch: ignore

checks/cruft.pm
@@ -34,6 +34,7 @@ use Carp qw(croak);
 use File::Basename qw(basename);
 use List::MoreUtils qw(any);
 use Path::Tiny;
+use Unicode::UTF8 qw(valid_utf8 decode_utf8);
 
 use Lintian::Data;
 use Lintian::Relation ();
@@ -345,9 +346,9 @@ our @EOL_TERMINATORS_FILES = qw(control changelog);
 
 # List of files to check for a trailing whitespace characters
 our @TRAILING_WHITESPACE_FILES = (
-    ['debian/changelog'        => qr,\s+\n$,],
-    ['debian/control'          => qr,\s+\n$,],
-    ['debian/rules'            => qr,[ ]+\n$,], # Allow trailing tabs in Make
+    ['debian/changelog'        => qr,\s+$,],
+    ['debian/control'          => qr,\s+$,],
+    ['debian/rules'            => qr,[ ]+$,], # Allow trailing tabs in Make
 );
 
 sub source {
@@ -396,24 +397,50 @@ sub source {
     }
 
     for my $file (@TRAILING_WHITESPACE_FILES) {
+
         my $path = $processable->patched->resolve_path($file->[0]);
-        next if not $path or not $path->is_open_ok;
-        open(my $fd, '<', $path->unpacked_path);
-        my @empty_lines;
-        while (my $line = <$fd>) {
-            if ($line eq "\n") {
-                push @empty_lines, $.;
-                next;
+        next
+          unless $path && $path->is_open_ok;
+
+        my $bytes = path($path->unpacked_path)->slurp;
+        next
+          unless valid_utf8($bytes);
+
+        my $contents = decode_utf8($bytes);
+        my @lines = split(/\n/, $contents, -1);
+
+        my @trailing_whitespace;
+        my @empty_at_end;
+
+        my $position = 1;
+        for my $line (@lines) {
+
+            push(@trailing_whitespace, $position)
+              if $line =~ $file->[1];
+
+            # keeps track of any empty lines at the end
+            if (length $line) {
+                @empty_at_end = ();
+            } else {
+                push(@empty_at_end, $position);
             }
-            @empty_lines = (); # reset; line is not empty
-            $self->tag('file-contains-trailing-whitespace', "$path (line $.)")
-              if ($line =~ $file->[1]);
+
+        } continue {
+            ++$position;
         }
-        close($fd);
-        for my $num (@empty_lines) {
-            $self->tag('file-contains-trailing-whitespace',
-                "$path (line $num)");
+
+        # require a newline at end and remove it
+        if (scalar @empty_at_end && $empty_at_end[-1] == scalar @lines) {
+            pop @empty_at_end;
+        } else {
+            $self->tag('no-newline-at-end', $path);
         }
+
+        push(@trailing_whitespace, @empty_at_end);
+
+        $self->tag('file-contains-trailing-whitespace', "$path (line $_)")
+          for @trailing_whitespace;
+
     }
 
     if (my $pycompat = $processable->patched->resolve_path('debian/pycompat')){

t/tags/checks/cruft/legacy-filenames/build-spec/debian/rules
@@ -94,10 +94,10 @@ binary-indep:
 	install -d debian/tmp/usr/share/doc/filenames
 	touch debian/tmp/usr/share/doc/filenames/README.macosx
 
+	echo foo > debian/tmp/usr/share/doc/filenames/bokmÃ¥l
+	echo foo > debian/tmp/usr/share/doc/filenames/bokm\\Ã¥l
 	echo foo > debian/tmp/usr/share/doc/filenames/bokmål
 	echo foo > debian/tmp/usr/share/doc/filenames/bokm\\ål
-	echo foo > debian/tmp/usr/share/doc/filenames/bokm�l
-	echo foo > debian/tmp/usr/share/doc/filenames/bokm\\�l
 
 	echo foo > debian/tmp/usr/share/doc/filenames/Thumbs.db
 	echo foo > debian/tmp/usr/share/doc/filenames/.DS_Store