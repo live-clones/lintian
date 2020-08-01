96559fc8507364a91794c68b8c557c0457faebd5
Felix Lechner <felix.lechner@lease-up.com>
2020-07-31 17:43:08 -0700

    Provide safe_qx based on IPC::Run3; add libipc-run3-perl as Depends and Build-Depends in d/control.
    Helps with the gradual conversion of Lintian to IPC::Run3.
    
    The test runner will continue to use IO::Async.
    
    Gbp-Dch: ignore

---
+++ lib/Lintian/IPC/Run3.pm
@@ -0,0 +1,101 @@
+# Hey emacs! This is a -*- Perl -*- script!
+#
+# Lintian::IPC::Run3 -- Perl utility functions for lintian
+#
+# Copyright © 2020 Felix Lechner
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, you can find it on the World Wide
+# Web at http://www.gnu.org/copyleft/gpl.html, or write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+package Lintian::IPC::Run3;
+
+use v5.20;
+use warnings;
+use utf8;
+use autodie;
+
+use Exporter qw(import);
+
+our @EXPORT_OK;
+
+BEGIN {
+
+    @EXPORT_OK = qw(
+      safe_qx
+    );
+}
+
+use Carp qw(croak);
+use IPC::Run3;
+
+use constant EMPTY => q{};
+
+=head1 NAME
+
+Lintian::IPC::Run3 - process functions based on IPC::Run3
+
+=head1 SYNOPSIS
+
+ use Lintian::IPC::Run3 qw(safe_qx);
+
+=head1 DESCRIPTION
+
+This module contains process functions based on IPC::Run3.
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item C<safe_qx(@cmd)>
+
+Emulates the C<qx()> operator but with array argument only.
+
+=cut
+
+sub safe_qx {
+    my @command = @_;
+
+    my $stdout;
+    my $stderr;
+
+    run3(\@command, undef, \$stdout, \$stderr);
+
+    my $exitcode = $?;
+    my $status = ($exitcode >> 8);
+
+    $? = $status;
+
+    return $stdout . $stderr
+      if $?;
+
+    return $stdout;
+}
+
+=back
+
+=head1 SEE ALSO
+
+lintian(1)
+
+=cut
+
+1;
+
+# Local Variables:
+# indent-tabs-mode: nil
+# cperl-indent-level: 4
+# End:
+# vim: syntax=perl sw=4 sts=4 sr et
