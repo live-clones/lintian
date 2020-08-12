5d4f56a671f427b5a1313fac87addc06764dc955
Felix Lechner <felix.lechner@lease-up.com>
2020-08-12 10:52:06 -0700

    Move to all executables not meant for shipping to ./private.
    For a brief while, some programs from t/bin were shipped to users in ./bin.
    End users did not need them, and could potentially be confused by their
    presence.
    
    Those programs can now be found in the source tree in ./private and are no
    longer shipped.

--- lib/Test/Lintian/Filter.pm
+++ lib/Test/Lintian/Filter.pm
@@ -327,8 +327,8 @@ sub find_all_tags {
     # read tags from specification
     my $temp = Path::Tiny->tempfile;
     die "tagextract failed: $!"
-      if
-      system('bin/tagextract', '-f', 'EWI', "$testpath/tags",$temp->stringify);
+      if system('private/tagextract', '-f', 'EWI', "$testpath/tags",
+        $temp->stringify);
     my @lines = $temp->lines_utf8({ chomp => 1 });
 
     my $csv = Text::CSV->new({ sep_char => '|' });
