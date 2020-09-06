9b79d2e6983a01cf551ede9e50c462ae6619150a
Peter Wienemann <fossdev@posteo.de>
2020-09-06 16:55:05 +0200

    Add new classification tag number-of-patches

--- checks/debian/patches.pm
+++ checks/debian/patches.pm
@@ -60,6 +60,19 @@ sub source {
     $patch_series
       = $self->processable->patched->resolve_path('debian/patches/series');
 
+    # Count number of patches
+    my $num_patches = 0;
+    open(my $fd, '<', $patch_series->unpacked_path);
+    while (my $line = <$fd>) {
+        chomp $line;
+        # Remove comments
+        $line =~ s/(?:^|\s+)#.*$//;
+	$num_patches++
+          if $line =~ /(?:^|\s+)\S+.*$/;
+    }
+    close($fd);
+    $self->tag('number-of-patches', $num_patches);
+
     push(@patch_system, 'dpatch')
       if $build_deps->implies('dpatch');
 
