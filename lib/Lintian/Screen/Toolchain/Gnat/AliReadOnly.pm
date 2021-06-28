e16ab6049e172dc18bd2e0a0e42025501376e122
Felix Lechner <felix.lechner@lease-up.com>
2021-06-28 15:26:13 -0700

    Exempt GNAT's read-only .ali files from non-standard-file-perm. (Closes: #986400)
    This is the second exemption granted using a new, generalized facility
    provided for that purpose in commit 8c3d5875.
    
    The new mechanism makes it easy to track, update, and revert
    exemptions when they are no longer needed. (We did take note, however,
    that this one is required by policy.)
    
    Please check the algorithm and the reasoning provided, as well as your
    contact information.
    
    As a example, the package libadacgi4-dev_1.6-26+b1_amd64.deb now
    produces the following output (when the option to show overrides is
    turned on):
    
    $ bin/lintian --show-overrides /mirror/debian/pool/main/a/adacgi/libadacgi4-dev_1.6-26+b1_amd64.deb
    N: screen toolchain/gnat/ali-read-only
    M: libadacgi4-dev: non-standard-file-perm usr/lib/x86_64-linux-gnu/ada/adalib/adacgi/cgi.ali 0444 != 0644
    N: screen toolchain/gnat/ali-read-only
    M: libadacgi4-dev: non-standard-file-perm usr/lib/x86_64-linux-gnu/ada/adalib/adacgi/ustrings.ali 0444 != 0644
    
    Thanks to Nicolas Boulenguez for bringing the matter to our attention!

---
+++ lib/Lintian/Screen/Toolchain/Gnat/AliReadOnly.pm
@@ -0,0 +1,83 @@
+# Copyright © 2021 Felix Lechner
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, you can find it on the World Wide
+# Web at http://www.gnu.org/copyleft/gpl.html, or write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+package Lintian::Screen::Toolchain::Gnat::AliReadOnly;
+
+use v5.20;
+use warnings;
+use utf8;
+
+use Moo;
+use namespace::clean;
+
+with 'Lintian::Screen';
+
+=head1 NAME
+
+Lintian::Screen::Toolchain::Gnat::AliReadOnly
+
+=head1 SYNOPSIS
+
+ use Moo;
+ use namespace::clean;
+
+=head1 DESCRIPTION
+
+Screen out hints provoked by C<GNAT> read-only file permissions.
+
+=head1 INSTANCE METHODS
+
+=over 4
+
+=item suppress
+
+=cut
+
+sub suppress {
+    my ($self, $processable, $context) = @_;
+
+    return 1
+      if $context
+      =~ m{^ usr/lib/ [^/]+ /ada/adalib/ [^/]+ / [^/]+ [.] ali \b }x
+      && ( $processable->type eq 'binary'
+        || $processable->type eq 'udeb')
+      && $processable->name =~ /-dev$/
+      && $processable->relation('strong')->implies('gnat');
+
+    return 0;
+}
+
+=back
+
+=head1 AUTHOR
+
+Originally written by Felix Lechner <felix.lechner@lease-up.com> for Lintian.
+
+=head1 SEE ALSO
+
+lintian(1)
+
+=cut
+
+1;
+
+# Local Variables:
+# indent-tabs-mode: nil
+# cperl-indent-level: 4
+# End:
+# vim: syntax=perl sw=4 sts=4 sr et
--- tags/n/non-standard-file-perm.tag
+++ tags/n/non-standard-file-perm.tag
@@ -4,3 +4,29 @@ Check: files/permissions
 Explanation: The file has a mode different from 0644. In some cases this is
  intentional, but in other cases this is a bug.
 See-Also: policy 10.9
+
+Screen: toolchain/gnat/ali-read-only
+Petitioners: Nicolas Boulenguez <nicolas@debian.org>
+Reason: In GNAT, the compiler also deals with dependencies and rebuild order.
+ The <tt>.ali</tt> files contain the dependency information required to detect
+ if a <tt>.o</tt> is more recent than the closure of all sources it depends
+ upon, or if it should be rebuilt.
+ .
+ By convention, a read-only <tt>.ali</tt> file tells <tt>GNAT</tt> to fail if
+ the <tt>.o</tt> is obsolete or unavailable, instead of attempting to rebuild.
+ This is recommended for packaged libraries (the <tt>.so</tt> or <tt>.a</tt>
+ are available but not the <tt>.o</tt> files).
+ .
+ This convention may seem bizarre according to modern standards, but it
+ has been in use for 25 years, so Adacore would probably need a
+ compelling reason to break it.
+ .
+ See also Debian Policy 8.4, which explicitly requires this:
+ .
+ If the package provides Ada Library Information (<tt>*.ali</tt>) files for use
+ with <tt>GNAT</tt>, these files must be installed read-only (mode 0444) so that
+ <tt>GNAT>/tt> will not attempt to recompile them. This overrides the normal
+ file mode requirements given in "Permissions and owners."
+See-Also:
+ policy 8.4,
+ Bug#986400
