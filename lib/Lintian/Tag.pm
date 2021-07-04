live-clones/lintian

commit 8c3d587559cfbdf5dd41e9ba1f66c9ab52de577e

Provide a generalized method to grant well-documented and isolated exemptions.
Some toolchains build packages that provoke the same hint in many
related packages. Often, the tooling is well-respected or has been
around for a long time. Either way, a maintainer can usually do
nothing about it. The hints are therefore perceived as false
positives.

Some teams will install overrides, while others approach Lintian with
requests for exemptions. Those were usually applied in the Lintian
check, namely at the point of detection. Unfortunately that cluttered
the code and sometimes introduced bugs.

This commit provides a generalized and well-documented way to grant
exemptions. Most important the exemptions, which are called "screens,"
are now separate and isolated pieces of code.

A hint that was masked by a screen is treated much like one with an
override, except that it is shown with the letter M (instead of O). An
automatic comment shows the name of the screen that generated the
mask.

Going forward, exemptions can be counted. They also carry enough
information to be reversed in an educated manner when the time is
right.

To aid with that effort, the screens provide the names and email
addresses of the petitioners, a summary of the reasoning for the
exemption, and further references (usually a bug number).

lib/Lintian/Tag.pm
@@ -26,7 +26,8 @@ use utf8;
 
 use Carp qw(croak);
 use Const::Fast;
-use List::SomeUtils qw(none);
+use Email::Address::XS;
+use List::SomeUtils qw(none first_value);
 use Unicode::UTF8 qw(encode_utf8);
 
 use Lintian::Deb822::File;
@@ -163,6 +164,11 @@ has renamed_from => (
     coerce => sub { my ($arrayref) = @_; return ($arrayref // []); },
     default => sub { [] });
 
+has screens => (
+    is => 'rw',
+    coerce => sub { my ($arrayref) = @_; return ($arrayref // []); },
+    default => sub { [] });
+
 has profile => (is => 'rw');
 
 =item load(PATH)
@@ -179,10 +185,8 @@ sub load {
 
     my $deb822 = Lintian::Deb822::File->new;
     my @sections = $deb822->read_file($tagpath);
-    croak encode_utf8("$tagpath does not have exactly one paragraph")
-      unless scalar @sections == 1;
 
-    my $fields = $sections[0];
+    my $fields = shift @sections;
 
     $self->check($fields->value('Check'));
     $self->name_spaced($fields->value('Name-Spaced') eq 'yes');
@@ -199,11 +203,8 @@ sub load {
 
     $self->explanation($fields->text('Explanation') || $fields->text('Info'));
 
-    my @see_also
-      = split(/,/, $fields->value('See-Also') || $fields->value('Ref'));
-
-    # trim both ends of each
-    s/^\s+|\s+$//g for @see_also;
+    my @see_also = $fields->trimmed_list('See-Also', qr{,})
+      || $fields->trimmed_list('Ref', qr{,});
 
     my @markdown = map { $self->markdown_citation($_) } @see_also;
     $self->see_also(\@markdown);
@@ -214,6 +215,45 @@ sub load {
       unless length $self->name;
 
     $self->effective_severity($self->visibility);
+
+    my @screens;
+    for my $section (@sections) {
+
+        my $screen_name = $section->value('Screen');
+
+        my $relative = $screen_name;
+        $relative =~ s{^([[:lower:]])}{\U$1};
+        $relative =~ s{/([[:lower:]])}{/\U$1}g;
+        $relative =~ s{-([[:lower:]])}{\U$1}g;
+
+        my @candidates = map { "$_/screens/$relative.pm" }
+          @{$self->profile->safe_include_dirs};
+
+        my $absolute = first_value { -e } @candidates;
+        require $absolute;
+
+        my $module = $relative;
+        $module =~ s{/}{::}g;
+
+        my $screen = "Lintian::Screen::$module"->new;
+
+        $screen->name($screen_name);
+
+        my @petitioners
+          = Email::Address::XS->parse($section->value('Petitioners'));
+        $screen->petitioners(\@petitioners);
+
+        $screen->reason($section->text('Reason'));
+
+        my @see_also_screen = $section->trimmed_list('See-Also', qr{,});
+        my @markdown_screen
+          = map { $self->markdown_citation($_) } @see_also_screen;
+        $screen->see_also(\@markdown_screen);
+
+        push(@screens, $screen);
+    }
+
+    $self->screens(\@screens);
 
     return;
 }

lib/Lintian/Group.pm
@@ -29,7 +29,7 @@ use Cwd;
 use Devel::Size qw(total_size);
 use File::Spec;
 use List::Compare;
-use List::SomeUtils qw(none uniq firstval);
+use List::SomeUtils qw(any none uniq firstval);
 use POSIX qw(ENOENT);
 use Time::HiRes qw(gettimeofday tv_interval);
 use Time::Piece;
@@ -45,6 +45,7 @@ use Moo;
 use namespace::clean;
 
 const my $EMPTY => q{};
+const my $SPACE => q{ };
 const my $UNDERSCORE => q{_};
 
 const my $EXTRA_VERBOSE => 3;
@@ -365,6 +366,37 @@ sub process {
             say {*STDERR} encode_utf8("$procid,check/$name,$raw_res")
               if $option->{'perf-output'};
         }
+
+        my @crossing;
+
+        my $hints = $processable->hints;
+        $processable->hints([]);
+
+        for my $hint (@{$hints}) {
+
+            next
+              if $hint->tag->show_always;
+
+            my @matches = grep { $_->suppress($processable, $hint->context) }
+              @{$hint->tag->screens};
+            next
+              unless @matches;
+
+            my @sorted = sort { $a->name cmp $b->hame } @matches;
+
+            push(@crossing,
+                    $hint->tag->name
+                  . $SPACE
+                  . join($SPACE, map { $_->name } @sorted))
+              if @sorted > 1;
+
+            my $mask = $sorted[0];
+            $hint->mask($mask);
+        }
+
+        $processable->hints($hints);
+
+        $processable->hint('crossing-screens', $_) for @crossing;
 
         my %used_overrides;
 

lib/Lintian/Output/EWI.pm
@@ -47,6 +47,7 @@ const my %COLORS => (
     'P' => 'green',
     'C' => 'blue',
     'O' => 'bright_black',
+    'M' => 'bright_black',
 );
 
 const my %CODE_PRIORITY => (
@@ -57,6 +58,7 @@ const my %CODE_PRIORITY => (
     'X' => 70,
     'C' => 80,
     'O' => 90,
+    'M' => 100,
 );
 
 const my %TYPE_PRIORITY => (
@@ -173,6 +175,7 @@ sub print_hint {
 
     my $code = $tag->code;
     $code = 'O' if defined $hint->override;
+    $code = 'M' if defined $hint->mask;
 
     my $tag_color = $COLORS{$code};
 
@@ -194,6 +197,9 @@ sub print_hint {
         say encode_utf8('N: ' . $self->_quote_print($_))
           for @{$hint->override->{comments}};
     }
+
+    say encode_utf8('N: screen ' . $hint->mask->name)
+      if defined $hint->mask;
 
     my $type = $EMPTY;
     $type = $SPACE . $hint->processable->type

lib/Lintian/Screen.pm
@@ -1,4 +1,4 @@
-# Copyright © 2019 Felix Lechner
+# Copyright © 2021 Felix Lechner
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,71 +16,48 @@
 # Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-package Lintian::Hint;
+package Lintian::Screen;
 
 use v5.20;
 use warnings;
 use utf8;
 
-use Const::Fast;
-
 use Moo::Role;
 use namespace::clean;
 
-const my $EMPTY => q{};
-const my $SPACE => q{ };
-
 =head1 NAME
 
-Lintian::Hint -- Common facilities for Lintian tags found and to be issued
+Lintian::Screen -- Common facilities for Lintian screens
 
 =head1 SYNOPSIS
 
  use Moo;
  use namespace::clean;
 
- with 'Lintian::Hint';
+ with('Lintian::Screen');
 
 =head1 DESCRIPTION
 
-Common facilities for Lintian tags found and to be issued
+A class for masking Lintian tags after they are issued
 
 =head1 INSTANCE METHODS
 
 =over 4
 
-=item arguments
-=item tag
-=item override
-=item mask
-=item processable
+=item name
 
-=item context
+=item petitioners
 
-Calculate the string representation commonly referred to as 'context'.
+=item reason
+
+=item see_also
 
 =cut
 
-has arguments => (is => 'rw', default => sub { [] });
-has tag => (is => 'rw');
-has override => (is => 'rw');
-has mask => (is => 'rw');
-has processable => (is => 'rw');
-
-sub context {
-    my ($self) = @_;
-
-    # skip empty arguments
-    my @relevant = grep { length } @{$self->arguments};
-
-    # concatenate with spaces
-    my $context = join($SPACE, @relevant) // $EMPTY;
-
-    # escape newlines; maybe add others
-    $context =~ s/\n/\\n/g;
-
-    return $context;
-}
+has name => (is => 'rw', default => sub { {} });
+has petitioners => (is => 'rw', default => sub { {} });
+has reason => (is => 'rw', default => sub { {} });
+has see_also => (is => 'rw', default => sub { {} });
 
 =back
 

screens
@@ -1,0 +1,1 @@
+lib/Lintian/Screen

t/scripts/harness/tag-coverage.t
@@ -48,6 +48,7 @@ const my $NEWLINE => qq{\n};
 
 my @known_missing = (qw(
       changed-by-invalid-for-derivative
+      crossing-screens
       debian-files-list-in-source
       debian-rules-not-executable
       embedded-pear-module

t/scripts/profiles-coverage.t
@@ -50,9 +50,9 @@ for my $tag_path (@tag_paths) {
     my $deb822 = Lintian::Deb822::File->new;
     my @sections = $deb822->read_file($tag_path);
 
-    BAIL_OUT("$tag_path does not have exactly one paragraph")
-      if (scalar(@sections) != 1);
-    my $header = $sections[0];
+    BAIL_OUT("$tag_path does not have at least one paragraph")
+      unless @sections;
+    my $header = shift @sections;
 
     ok($header->declares('Tag'), "Field Tag exists in $tag_path");
     ok($header->declares('Check'), "Field Check exists in $tag_path");

t/scripts/tags/fields.t
@@ -31,7 +31,7 @@ use utf8;
 use Const::Fast;
 use File::Find::Rule;
 use IPC::Run3;
-use List::Util qw(all);
+use List::SomeUtils qw(true);
 use Path::Tiny;
 use Test::More;
 
@@ -66,25 +66,33 @@ $profile->load(undef, undef, 0);
 
 for my $tagpath (@tagpaths) {
 
-    # test for duplicate fields
-    my %count;
+    my $contents = path($tagpath)->slurp_utf8;
+    my @parts = split(m{\n\n}, $contents);
 
-    my @lines = path($tagpath)->lines;
-    for my $line (@lines) {
-        my ($field) = $line =~ qr/^(\S+):/;
-        $count{$field} += 1
-          if defined $field;
+    # test for duplicate fields
+    my $duplicates = 0;
+
+    for my $part (@parts) {
+        my %count;
+
+        my @lines = split(/\n/, $part);
+        for my $line (@lines) {
+            my ($field) = $line =~ qr/^(\S+):/;
+            $count{$field} += 1
+              if defined $field;
+        }
+
+        $duplicates += true { $count{$_} > 1 } keys %count;
     }
 
-    ok((all { $count{$_} == 1 } keys %count),
-        "No duplicate fields in $tagpath");
+    is($duplicates, 0, "No duplicate fields in $tagpath");
 
     my $deb822 = Lintian::Deb822::File->new;
 
     my @sections = $deb822->read_file($tagpath);
-    is(scalar @sections, 1, "Tag in $tagpath has exactly one section");
+    ok(@sections >= 1, "Tag in $tagpath has at least one section");
 
-    my $fields = $sections[0] // {};
+    my $fields = shift @sections;
 
     # tag has a name
     my $tagname = $fields->value('Tag');

commit a0f4f3595b9f3ae532a9ff6987c5d3206fe13156

Evaluate tag references as arrays and not scalars.
This bug fixed here was introduced in the recent commit 8c3d5875.
Annotated output looked like this: (Note "Refer to 1 for details.")

W: libmarkdown-php: embedded-php-library usr/share/php/markdown.php please use php-markdown
N:
N:   This package appears to contain an embedded copy of a PHP library. Please
N:   depend on the respective package providing the library and make sure it can
N:   be found by the scripts via the include_path.
N:
N:   Refer to 1 for details.
N:
N:   Severity: warning
N:
N:   Check: languages/php/embedded

Gbp-Dch: ignore

lib/Lintian/Tag.pm
@@ -203,8 +203,9 @@ sub load {
 
     $self->explanation($fields->text('Explanation') || $fields->text('Info'));
 
-    my @see_also = $fields->trimmed_list('See-Also', qr{,})
-      || $fields->trimmed_list('Ref', qr{,});
+    my @see_also = $fields->trimmed_list('See-Also', qr{,});
+    @see_also = $fields->trimmed_list('Ref', qr{,})
+      unless @see_also;
 
     my @markdown = map { $self->markdown_citation($_) } @see_also;
     $self->see_also(\@markdown);