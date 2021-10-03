live-clones/lintian

commit 9bc560a62571f2f1a70ce7044093c42ff14e3efa

Implement the multiarch selector :any in package relationships. (Closes: #994902)
The previous detection logic for these resolver hints was possbly reversed. As
an example, the prerequisite 'make' seems to have implied 'make:any'.

The preceeding five commits show the extensive preparatory work that was needed
for this fix.

We built the example from tha bug report, postgresql-common, from Git using
commit 38ec687b. Lintian no longer produces the offending tag:

➤ ../lintian/git/bin/lintian postgresql-server-dev-all_229_amd64.deb
I: postgresql-server-dev-all: package-contains-documentation-outside-usr-share-doc usr/share/postgresql-common/dh_make_pgxs/debian/copyright

For comparison, here is the output that prompted the bug report:

➤ ../lintian/git/bin/lintian postgresql-server-dev-all_229_amd64.deb
E: postgresql-server-dev-all: missing-dep-for-interpreter make => make | build-essential | dpkg-dev (usr/share/postgresql-common/dh_make_pgxs/debian/rules) /usr/bin/make
I: postgresql-server-dev-all: package-contains-documentation-outside-usr-share-doc usr/share/postgresql-common/dh_make_pgxs/debian/copyright

Thank you to Christoph Berg for bringing the matter to our attention!

lib/Lintian/Relation/Predicate.pm
@@ -339,28 +339,28 @@ sub implies {
     # Other than that, we would need to know that the package has the
     # field "Multi-arch: allowed", but we cannot check that here.  So
     # we assume that it is okay.
-    #
+
+    # pkg:any implies pkg (but the reverse is not true).
+    return undef
+      if length $self->multiarch_acceptor
+      && !length $other->multiarch_acceptor
+      && $self->multiarch_acceptor ne 'any';
+
+    # TODO: Review this case.  Are there cases where other cannot
+    # disprove self due to the ":any"-qualifier?  For now, we
+    # assume there are no such cases.
+    return undef
+      if !length $self->multiarch_acceptor
+      && length $other->multiarch_acceptor;
+
     # For now assert that only the identity holds.  In practise, the
     # "pkg:X" (for any valid value of X) seems to imply "pkg:any",
     # fixing that is a TODO (because version clauses complicates
     # matters)
-    if (length $self->multiarch_acceptor) {
-        # Assume the identity to hold
-        return undef
-          unless length $other->multiarch_acceptor
-          && $self->multiarch_acceptor eq $other->multiarch_acceptor;
-
-    } elsif (length $other->multiarch_acceptor) {
-
-        return undef
-          unless $other->multiarch_acceptor eq 'any';
-
-        # pkg:any implies pkg (but the reverse is not true).
-        #
-        # TODO: Review this case.  Are there cases where other cannot
-        # disprove self due to the ":any"-qualifier?  For now, we
-        # assume there are no such cases.
-    }
+    return undef
+      if length $self->multiarch_acceptor
+      && length $other->multiarch_acceptor
+      && $self->multiarch_acceptor ne $other->multiarch_acceptor;
 
   # Now, down to version.  The implication is true if self's clause is stronger
   # than other's, or is equivalent.

t/recipes/checks/scripts/scripts-control-interpreters/eval/hints
@@ -5,10 +5,13 @@ scripts-control-interpreters-python (binary): unusual-control-interpreter contro
 scripts-control-interpreters-python (binary): maintainer-script-interpreter control/preinst /usr/bin/python3
 scripts-control-interpreters-python (binary): maintainer-script-interpreter control/postinst /usr/bin/python3
 scripts-control-interpreters-python (binary): control-interpreter-without-predepends /usr/bin/python3 (preinst) python3:any | python3-minimal:any
+scripts-control-interpreters-python (binary): control-interpreter-without-depends /usr/bin/python3 (postinst) python3:any | python3-minimal:any
 scripts-control-interpreters-prepython (binary): unusual-control-interpreter control/preinst /usr/bin/python3
 scripts-control-interpreters-prepython (binary): unusual-control-interpreter control/postinst /usr/bin/python3
 scripts-control-interpreters-prepython (binary): maintainer-script-interpreter control/preinst /usr/bin/python3
 scripts-control-interpreters-prepython (binary): maintainer-script-interpreter control/postinst /usr/bin/python3
+scripts-control-interpreters-prepython (binary): control-interpreter-without-predepends /usr/bin/python3 (preinst) python3:any | python3-minimal:any
+scripts-control-interpreters-prepython (binary): control-interpreter-without-depends /usr/bin/python3 (postinst) python3:any | python3-minimal:any
 scripts-control-interpreters-paths (binary): wrong-path-for-interpreter /usr/bin/bash != /bin/bash (control/postinst)
 scripts-control-interpreters-paths (binary): wrong-path-for-interpreter /bin/python3 != /usr/bin/python3 (control/prerm)
 scripts-control-interpreters-paths (binary): unusual-control-interpreter control/prerm /bin/python3

t/scripts/Lintian/Relation/04-multiarch.t
@@ -9,52 +9,45 @@ use Lintian::Relation;
 my $orig = 'pkgA:any, pkgB, pkgC:i386';
 my $relation = Lintian::Relation->new->load($orig);
 
-ok($relation->implies('pkgA:any'),   'identity implies [pkgA]');
+ok($relation->implies('pkgA:any'),   'pkgA:any implies pkgA:any');
 
-ok($relation->implies('pkgB'),       'identity implies [pkgB]');
+ok($relation->implies('pkgB'),       'pkgB implies pkgB');
 
-ok(!$relation->implies('pkgC'),      'archless implies [pkgC]');
-ok($relation->implies('pkgC:i386'),  'identity implies [pkgC]');
+ok(!$relation->implies('pkgC'),      'pkgC:i386 does not imply pkgC');
+ok($relation->implies('pkgC:i386'),  'pkgC:i386 implies pkgC:i386');
 
-ok($relation->implies('pkgB:any'),   'arch any implies [pkgB]');
+ok(!$relation->implies('pkgB:any'),  'pkgB does not imply pkgB:any');
 
-TODO: {
-    local $TODO = ':X => :Y cases are not implemented (in general)';
+ok($relation->implies('pkgA'),       'pkgA:any implies pkgA');
 
-    ok($relation->implies('pkgA'),       'archless implies [pkgA]');
+ok(!$relation->implies('pkgC:any'),  'pkgC:i386 does not imply pkgC:any');
 
-    ok($relation->implies('pkgC:any'),   'arch any implies [pkgC]');
-}
-
-is($relation->to_string, $orig,          'reconstituted eq original');
+is($relation->to_string, $orig,      'reconstituted eq original');
 
 my @dups1 = Lintian::Relation->new->load('pkgD, pkgD:any')->duplicates;
-
-my @dups2 = Lintian::Relation->new->load('pkgD:i386, pkgD:any')->duplicates;
-
-my @dups3 = Lintian::Relation->new->load('pkgD:i386, pkgD')->duplicates;
-
-my @dups4
-  = Lintian::Relation->new->load('pkgD:i386, pkgD:i386 (>= 1.0)')->duplicates;
-
 is_deeply(\@dups1,[['pkgD', 'pkgD:any']],'pkgD and pkgD:any are dups');
-
-is_deeply(\@dups3, [],'pkgD:i386 and pkgD are not dups');
-is_deeply(
-    \@dups4,
-    [['pkgD:i386', 'pkgD:i386 (>= 1.0)']],
-    'Can detect pkgD:i386 dups'
-);
 
 TODO: {
     local $TODO = ':X => :Y cases are not implemented (in general)';
 
+    my @dups2= Lintian::Relation->new->load('pkgD:i386, pkgD:any')->duplicates;
     is_deeply(
         \@dups2,
         [['pkgD:i386', 'pkgD:any']],
         'pkgD:i386 and pkgD:any are dups'
     );
 }
+
+my @dups3 = Lintian::Relation->new->load('pkgD:i386, pkgD')->duplicates;
+is_deeply(\@dups3, [],'pkgD:i386 and pkgD are not dups');
+
+my @dups4
+  = Lintian::Relation->new->load('pkgD:i386, pkgD:i386 (>= 1.0)')->duplicates;
+is_deeply(
+    \@dups4,
+    [['pkgD:i386', 'pkgD:i386 (>= 1.0)']],
+    'Can detect pkgD:i386 dups'
+);
 
 # Local Variables:
 # indent-tabs-mode: nil

lib/Lintian/Check/Languages/Python.pm
@@ -232,7 +232,8 @@ sub installable {
         my $dep = $processable->relation($field);
       FIELD: for my $py2 (@PYTHON2) {
             for my $py3 (@PYTHON3) {
-                if ($dep->implies("$py2:any") and $dep->implies("$py3:any")) {
+                # do not look for :any here; too narrow
+                if ($dep->implies($py2) && $dep->implies($py3)) {
                     $self->hint('depends-on-python2-and-python3',
                         "$field: $py2, [..], $py3");
                     last FIELD;
@@ -254,10 +255,12 @@ sub installable {
     if ($pkg !~ /^python[23]?-/ and none { $_ eq $pkg } @PYTHON2) {
         for my $field (@FIELDS) {
             for my $dep (@PYTHON2) {
+
+                # do not look for :any here; too narrow
                 $self->hint(
                     'dependency-on-python-version-marked-for-end-of-life',
                     "($field: $dep)")
-                  if $processable->relation($field)->implies("$dep:any");
+                  if $processable->relation($field)->implies($dep);
             }
         }
     }

lib/Lintian/Check/Languages/Python/BogusPrerequisites.pm
@@ -64,11 +64,10 @@ sub what_is_python {
 
         for my $unwanted (@WHAT_IS_PYTHON) {
 
-            $BOGUS_PREREQUISITES{$unwanted} = [
-                grep {
-                    $self->processable->relation($_)->implies("$unwanted:any")
-                } @fields
-            ];
+            # do not look for :any here; too narrow
+            $BOGUS_PREREQUISITES{$unwanted}
+              = [grep {$self->processable->relation($_)->implies($unwanted)}
+                  @fields];
         }
     }
 

commit 14d0dcd34ba0ab62389782c65e3a442a4b24cf8c

Lintian::Relation: Fix implication relationships with :any qualifier
Truth table (cells indicate whether the dependency on the left implies
the dependency at the top):

               pkg          pkg:i386     pkg:any
    pkg        yes          maybe        yes
    pkg:i386   maybe        yes          yes
    pkg:any    no           no           yes

where the relationships labelled "maybe" are implied if the dependency
happens to be found in an i386 package, and otherwise not implied.

This fixes a false positive, #995490, which could be seen in the
scripts-control-interpreters-python test package: a dependency on python3
implies a dependency on python3:any, so it is enough to be able to run
a "#!/usr/bin/python3" script.

Closes: #995490
Fixes: 9bc560a6 "Implement the multiarch selector :any in package relationships"
Signed-off-by: Simon McVittie <smcv@debian.org>

lib/Lintian/Relation/Predicate.pm
@@ -340,27 +340,52 @@ sub implies {
     # field "Multi-arch: allowed", but we cannot check that here.  So
     # we assume that it is okay.
 
-    # pkg:any implies pkg (but the reverse is not true).
-    return undef
-      if length $self->multiarch_acceptor
-      && !length $other->multiarch_acceptor
-      && $self->multiarch_acceptor ne 'any';
-
-    # TODO: Review this case.  Are there cases where other cannot
-    # disprove self due to the ":any"-qualifier?  For now, we
-    # assume there are no such cases.
-    return undef
-      if !length $self->multiarch_acceptor
-      && length $other->multiarch_acceptor;
-
-    # For now assert that only the identity holds.  In practise, the
-    # "pkg:X" (for any valid value of X) seems to imply "pkg:any",
-    # fixing that is a TODO (because version clauses complicates
-    # matters)
-    return undef
-      if length $self->multiarch_acceptor
-      && length $other->multiarch_acceptor
-      && $self->multiarch_acceptor ne $other->multiarch_acceptor;
+    if (!length $self->multiarch_acceptor) {
+        # pkg trivially imples pkg.
+        #
+        # A missing qualifier has an effect similar to pkg:<arch> where
+        # <arch> is the architecture of the dependent package.
+        # We don't know what architecture that is, so assume pkg does not
+        # imply pkg:<other> for any specific architecture <other>.
+        #
+        # pkg *does* imply pkg:any, though, because if we have pkg for a
+        # specific architecture (even if we don't know in this context
+        # what that specific architecture is!), then it's certainly true
+        # that we have pkg for at least one architecture.
+        return undef
+          if length $other->multiarch_acceptor
+          && $other->multiarch_acceptor ne 'any';
+    } elsif ($self->multiarch_acceptor eq 'any') {
+        # pkg:any trivially implies pkg:any.
+        #
+        # It does not imply pkg:<arch> for any specific architecture <arch>,
+        # because pkg:any just means we have pkg for at least one
+        # architecture, without being able to say what that architecture is.
+        #
+        # It does not imply pkg, because that has an effect similar to
+        # pkg:<arch> where <arch> is the architecture of the dependent
+        # package.
+        return undef
+          if !length $other->multiarch_acceptor
+          || $other->multiarch_acceptor ne 'any';
+    } else {
+        # A specific architecture such as pkg:i386 trivially implies pkg:i386.
+        #
+        # It also implies pkg:any, because if you have the i386 version
+        # of pkg, it's certainly true that you have pkg for at least one
+        # architecture.
+        #
+        # It does not imply pkg:amd64, or in general any different
+        # architecture.
+        #
+        # It also does not imply pkg, because that has an effect similar
+        # to pkg:<arch> where <arch> is the architecture of the dependent
+        # package, which we do not know in this context.
+        return undef
+          if !length $other->multiarch_acceptor
+          || ( $other->multiarch_acceptor ne 'any'
+            && $other->multiarch_acceptor ne $self->multiarch_acceptor);
+    }
 
   # Now, down to version.  The implication is true if self's clause is stronger
   # than other's, or is equivalent.

t/recipes/checks/scripts/scripts-control-interpreters/eval/hints
@@ -5,13 +5,10 @@ scripts-control-interpreters-python (binary): unusual-control-interpreter contro
 scripts-control-interpreters-python (binary): maintainer-script-interpreter control/preinst /usr/bin/python3
 scripts-control-interpreters-python (binary): maintainer-script-interpreter control/postinst /usr/bin/python3
 scripts-control-interpreters-python (binary): control-interpreter-without-predepends /usr/bin/python3 (preinst) python3:any | python3-minimal:any
-scripts-control-interpreters-python (binary): control-interpreter-without-depends /usr/bin/python3 (postinst) python3:any | python3-minimal:any
 scripts-control-interpreters-prepython (binary): unusual-control-interpreter control/preinst /usr/bin/python3
 scripts-control-interpreters-prepython (binary): unusual-control-interpreter control/postinst /usr/bin/python3
 scripts-control-interpreters-prepython (binary): maintainer-script-interpreter control/preinst /usr/bin/python3
 scripts-control-interpreters-prepython (binary): maintainer-script-interpreter control/postinst /usr/bin/python3
-scripts-control-interpreters-prepython (binary): control-interpreter-without-predepends /usr/bin/python3 (preinst) python3:any | python3-minimal:any
-scripts-control-interpreters-prepython (binary): control-interpreter-without-depends /usr/bin/python3 (postinst) python3:any | python3-minimal:any
 scripts-control-interpreters-paths (binary): wrong-path-for-interpreter /usr/bin/bash != /bin/bash (control/postinst)
 scripts-control-interpreters-paths (binary): wrong-path-for-interpreter /bin/python3 != /usr/bin/python3 (control/prerm)
 scripts-control-interpreters-paths (binary): unusual-control-interpreter control/prerm /bin/python3

t/scripts/Lintian/Relation/04-multiarch.t
@@ -2,7 +2,7 @@
 
 use strict;
 use warnings;
-use Test::More tests => 12;
+use Test::More tests => 13;
 
 use Lintian::Relation;
 
@@ -12,32 +12,45 @@ my $relation = Lintian::Relation->new->load($orig);
 ok($relation->implies('pkgA:any'),   'pkgA:any implies pkgA:any');
 
 ok($relation->implies('pkgB'),       'pkgB implies pkgB');
+# pkgB implies pkgB:i386 if and only if the dependency is seen in an i386
+# package, but looking at the dependencies out of context like this, we
+# can't tell whether that's the case.
+ok(!$relation->implies('pkgB:i386'), 'pkgB does not imply pkgB:i386');
 
 ok(!$relation->implies('pkgC'),      'pkgC:i386 does not imply pkgC');
 ok($relation->implies('pkgC:i386'),  'pkgC:i386 implies pkgC:i386');
 
-ok(!$relation->implies('pkgB:any'),  'pkgB does not imply pkgB:any');
+# If we have pkgB:<arch> for some specific architecture, then it's certainly
+# true that we have pkgB for at least one architecture
+ok($relation->implies('pkgB:any'),   'pkgB implies pkgB:any');
 
-ok($relation->implies('pkgA'),       'pkgA:any implies pkgA');
+# pkgA:any does not imply pkgA, because if pkgA is Multi-Arch: allowed,
+# depending on pkgA is shorthand for pkgA:<arch> for some specific
+# architecture, whereas pkgA:any could be satisfied by an architecture
+# other than <arch>
+ok(!$relation->implies('pkgA'),      'pkgA:any does not imply pkgA');
 
-ok(!$relation->implies('pkgC:any'),  'pkgC:i386 does not imply pkgC:any');
+# If we have pkgC:i386, then it's certainly true that we have pkgC for at
+# least one architecture
+ok($relation->implies('pkgC:any'),   'pkgC:i386 implies pkgC:any');
 
 is($relation->to_string, $orig,      'reconstituted eq original');
 
+# { pkgD, pkgD:any } is equivalent to { pkgD }
 my @dups1 = Lintian::Relation->new->load('pkgD, pkgD:any')->duplicates;
 is_deeply(\@dups1,[['pkgD', 'pkgD:any']],'pkgD and pkgD:any are dups');
 
-TODO: {
-    local $TODO = ':X => :Y cases are not implemented (in general)';
+# { pkgD:i386, pkgD:any } is equivalent to { pkgD:i386 }
+my @dups2= Lintian::Relation->new->load('pkgD:i386, pkgD:any')->duplicates;
+is_deeply(
+    \@dups2,
+    [['pkgD:i386', 'pkgD:any']],
+    'pkgD:i386 and pkgD:any are dups'
+);
 
-    my @dups2= Lintian::Relation->new->load('pkgD:i386, pkgD:any')->duplicates;
-    is_deeply(
-        \@dups2,
-        [['pkgD:i386', 'pkgD:any']],
-        'pkgD:i386 and pkgD:any are dups'
-    );
-}
-
+# pkgD implies pkgD:i386 if and only if the dependency is seen in an i386
+# package, but looking at the dependencies out of context like this, we
+# can't tell whether that's the case.
 my @dups3 = Lintian::Relation->new->load('pkgD:i386, pkgD')->duplicates;
 is_deeply(\@dups3, [],'pkgD:i386 and pkgD are not dups');
 