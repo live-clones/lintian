8bda904598bc269ba669e6e2e6646a36cb5d52a2
Felix Lechner <felix.lechner@lease-up.com>
2021-11-16 04:29:18 -0800

    Bring the check languages/ocaml up to Lintian's latest code standards.
    Fixes the common path calculation for misplaced development files.
    
    Drops the custom tag limit. The command-line option --no-tag-display-limit
    provides a similar functionality and should be used instead.
    
    Gbp-Dch: ignore

--- lib/Lintian/Check/Languages/Ocaml.pm
+++ lib/Lintian/Check/Languages/Ocaml.pm
@@ -25,8 +25,7 @@ use warnings;
 use utf8;
 
 use Const::Fast;
-use File::Basename;
-use Path::Tiny;
+use List::SomeUtils qw(first_value);
 
 use Moo;
 use namespace::clean;
@@ -34,9 +33,7 @@ use namespace::clean;
 with 'Lintian::Check';
 
 const my $EMPTY => q{};
-
-# The maximum number of *.cmi files to show individually.
-const my $MAX_CMI => 3;
+const my $SLASH => q{/};
 
 has provided_o => (
     is => 'rw',
@@ -48,36 +45,21 @@ has provided_o => (
 
         for my $item (@{$self->processable->installed->sorted_list}) {
 
-            my $ar_info = $item->ar_info;
-            next
-              unless scalar keys %{$ar_info};
+            for my $count (keys %{$item->ar_info}) {
 
-            # ends in a slash
-            my $dirname = $item->dirname;
+                my $member = $item->ar_info->{$count}{name};
+                next
+                  unless length $member;
 
-            for my $count (keys %{$ar_info}) {
+                # dirname ends in a slash
+                my $virtual_path = $item->dirname . $member;
 
-                my $member = $ar_info->{$count}{name};
                 # Note: a .o may be legitimately in several different .a
-                $provided_o{"$dirname$member"} = $item->name
-                  if length $member;
+                $provided_o{$virtual_path} = $item->name;
             }
         }
 
         return \%provided_o;
-    });
-
-has is_lib_package => (
-    is => 'rw',
-    lazy => 1,
-    default => sub {
-        my ($self) = @_;
-
-        # is it a library package?
-        return 1
-          if $self->processable->name =~ /^lib/;
-
-        return 0;
     });
 
 has is_dev_package => (
@@ -103,96 +85,56 @@ has is_dev_package => (
         return 0;
     });
 
-# for libraries outside /usr/lib/ocaml
-has outside_number => (is => 'rw', default => 0);
-has outside_prefix => (is => 'rw');
+has development_files => (is => 'rw', default => sub { [] });
 
-# dangling .cmi files (we show only $MAX_CMI of them)
-has cmi_number => (is => 'rw', default => 0);
-
-# dev files in nondev package
-has dev_number => (is => 'rw', default => 0);
-has dev_prefix => (is => 'rw');
-
-# does the package provide a META file?
 has has_meta => (is => 'rw', default => 0);
 
 sub visit_installed_files {
     my ($self, $file) = @_;
 
+    my $no_extension = $file->name;
+    $no_extension =~ s{ [.] [^.]+ $}{}x;
+
     # For each .cmxa file, there must be a matching .a file (#528367)
-    $_ = $file;
-    if (s/\.cmxa$/.a/ && !$self->processable->installed->lookup($_)) {
-        $self->hint('ocaml-dangling-cmxa', $file);
-    }
+    $self->hint('ocaml-dangling-cmxa', $file)
+      if $file->name =~ m{ [.]cmxa $}x
+      && !$self->processable->installed->lookup($no_extension . '.a');
 
     # For each .cmxs file, there must be a matching .cma or .cmo file
     # (at least, in library packages)
-    if ($self->is_lib_package) {
-        $_ = $file;
-        if (   s/\.cmxs$/.cm/
-            && !$self->processable->installed->lookup("${_}a")
-            && !$self->processable->installed->lookup("${_}o")) {
-            $self->hint('ocaml-dangling-cmxs', $file);
-        }
-    }
+    $self->hint('ocaml-dangling-cmxs', $file)
+      if $file->name =~ m{ [.]cmxs $}x
+      && !$self->processable->installed->lookup($no_extension . '.cma')
+      && !$self->processable->installed->lookup($no_extension . '.cmo')
+      && $self->processable->name =~ /^lib/;
 
     # The .cmx counterpart: for each .cmx file, there must be a
     # matching .o file, which can be there by itself, or embedded in a
     # .a file in the same directory
-    $_ = $file;
-    if (   s/\.cmx$/.o/
-        && !$self->processable->installed->lookup($_)
-        && !(exists $self->provided_o->{$_})) {
-        $self->hint('ocaml-dangling-cmx', $file);
-    }
+    $self->hint('ocaml-dangling-cmx', $file)
+      if $file->name =~ m{ [.]cmx $}x
+      && !$self->processable->installed->lookup($no_extension . '.o')
+      && !exists $self->provided_o->{$no_extension . '.o'};
 
     # $somename.cmi should be shipped with $somename.mli or $somename.ml
-    $_ = $file;
-    if (   $self->is_dev_package
-        && s/\.cmi$/.ml/
-        && !$self->processable->installed->lookup("${_}i")
-        && !$self->processable->installed->lookup($_)) {
-        $self->cmi_number($self->cmi_number + 1);
-        if ($self->cmi_number <= $MAX_CMI) {
-            $self->hint('ocaml-dangling-cmi', $file);
-        }
-    }
+    $self->hint('ocaml-dangling-cmi', $file)
+      if $file->name =~ m{ [.]cmi $}x
+      && !$self->processable->installed->lookup($no_extension . '.mli')
+      && !$self->processable->installed->lookup($no_extension . '.ml')
+      && $self->is_dev_package;
 
-    # non-dev packages should not ship .cmi, .cmx or .cmxa files
-    if ($file =~ m/\.cm(i|xa?)$/) {
-        $self->dev_number($self->dev_number + 1);
-        if (defined $self->dev_prefix) {
-            my $dev_prefix = $self->dev_prefix;
-            chop $dev_prefix while ($file !~ m{^$dev_prefix});
-            $self->dev_prefix($dev_prefix);
-        } else {
-            $self->dev_prefix($file->name);
-        }
-    }
+    # .cma, .cmo and .cmxs are excluded because they can be plugins
+    push(@{$self->development_files}, $file->name)
+      if $file->name =~ m{ [.] cm (?: i | xa? ) $}x;
 
     # $somename.cmo should usually not be shipped with $somename.cma
-    $_ = $file;
-    if (s/\.cma$/.cmo/ && $self->processable->installed->lookup($_)) {
-        $self->hint('ocaml-stray-cmo', $file);
-    }
+    $self->hint('ocaml-stray-cmo', $file)
+      if $file->name =~ m{ [.]cma $}x
+      && $self->processable->installed->lookup($no_extension . '.cmo');
 
-    # development files outside /usr/lib/ocaml (.cmi, .cmx, .cmxa)
-    # .cma, .cmo and .cmxs are excluded because they can be plugins
-    if ($file =~ m/\.cm(i|xa?)$/ && $file !~ m{^usr/lib/ocaml/}) {
-        $self->outside_number($self->outside_number + 1);
-        if (defined $self->outside_prefix) {
-            my $outside_prefix = $self->outside_prefix;
-            chop $outside_prefix while ($file !~ m{^$outside_prefix});
-            $self->outside_prefix($outside_prefix);
-        } else {
-            $self->outside_prefix($file->name);
-        }
-    }
-
-    # If there is a META file, ocaml-findlib should be at least suggested.
+    # does the package provide a META file?
     $self->has_meta(1)
-      if $file =~ m{^usr/lib/ocaml/(.+/)?META(\..*)?$};
+      if $file =~ m{^ usr/lib/ocaml/ (?: .+ / )? META ([.] .* )? $}x;
 
     return;
 }
@@ -200,43 +142,79 @@ sub visit_installed_files {
 sub installable {
     my ($self) = @_;
 
+    my $depends = $self->processable->relation('all');
+
+    # If there is a META file, ocaml-findlib should at least be suggested.
+    $self->hint('ocaml-meta-without-suggesting-findlib')
+      if $self->has_meta
+      && $self->is_dev_package
+      && !$depends->satisfies('ocaml-findlib');
+
     if ($self->is_dev_package) {
-        # summary about .cmi files
-        if ($self->cmi_number > $MAX_CMI) {
-            my $plural = ($self->cmi_number - $MAX_CMI == 1) ? $EMPTY : 's';
-            $self->hint(
-                'ocaml-dangling-cmi',
-                ($self->cmi_number - $MAX_CMI),
-                "more file$plural not shown"
-            );
-        }
-        # summary about /usr/lib/ocaml
-        if ($self->outside_number) {
-            my $outside_number = $self->outside_number;
-            my $outside_prefix = dirname($self->outside_prefix);
-            my $plural = ($self->outside_number == 1) ? $EMPTY : 's';
-            $self->hint('ocaml-dev-file-not-in-usr-lib-ocaml',
-                "$outside_number file$plural in $outside_prefix");
-        }
-        if ($self->has_meta) {
-            my $depends = $self->processable->relation('all');
-            $self->hint('ocaml-meta-without-suggesting-findlib')
-              unless $depends->satisfies('ocaml-findlib');
-        }
-    } else {
-        # summary about dev files
-        if ($self->dev_number > 0) {
-            my $dev_number = $self->dev_number;
-            my $dev_prefix = dirname($self->dev_prefix);
-            my $plural = ($self->dev_number == 1) ? $EMPTY : 's';
-            $self->hint(
-                'ocaml-dev-file-in-nondev-package',
-                "$dev_number file$plural in $dev_prefix"
-            );
-        }
+
+        # development files outside /usr/lib/ocaml (.cmi, .cmx, .cmxa)
+        my @misplaced_files
+          = grep { !m{^ usr/lib/ocaml/ }x } @{$self->development_files};
+
+        my $count = scalar @misplaced_files;
+        my $plural = ($count == 1) ? $EMPTY : 's';
+
+        my $prefix = longest_common_prefix(@misplaced_files);
+
+        # strip trailing slash
+        $prefix =~ s{ / $}{}x
+          unless $prefix eq $SLASH;
+
+        $self->hint(
+            'ocaml-dev-file-not-in-usr-lib-ocaml',
+            "$count file$plural in $prefix"
+        );
+    }
+
+    # non-dev packages should not ship .cmi, .cmx or .cmxa files
+    if (!$self->is_dev_package
+        && @{$self->development_files}) {
+
+        my $count = scalar @{$self->development_files};
+        my $plural = ($count == 1) ? $EMPTY : 's';
+
+        my $prefix = longest_common_prefix(@{$self->development_files});
+
+        # strip trailing slash
+        $prefix =~ s{ / $}{}x
+          unless $prefix eq $SLASH;
+
+        $self->hint(
+            'ocaml-dev-file-in-nondev-package',
+            "$count file$plural in $prefix"
+        );
     }
 
     return;
+}
+
+sub longest_common_prefix {
+    my (@paths) = @_;
+
+    my %prefixes;
+
+    for my $path (@paths) {
+
+        my $truncated = $path;
+
+        # first operation drops the file name
+        while ($truncated =~ s{ / [^/]* $}{}x) {
+            ++$prefixes{$truncated};
+        }
+    }
+
+    my @by_descending_length = reverse sort keys %prefixes;
+
+    my $common = first_value { $prefixes{$_} == @paths } @by_descending_length;
+
+    $common ||= $SLASH;
+
+    return $common;
 }
 
 1;
