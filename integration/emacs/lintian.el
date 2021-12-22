addd6064625a6996b76afa0bd81cdb02eacd8ce9
Felix Lechner <felix.lechner@lease-up.com>
2021-12-22 05:15:33 -0800

    Provide rudimentary Emacs integration. (See: #968758)
    Provides the 'lintian-run-file' command to view packaging hints in Emacs. If a
    user's teminal cannot display OSC8 hyperlinks [1] it is a way to get live links
    to tag explanations.
    
    Unfortunately, the simple shr renderer used here does not process CSS. In many
    ways, the output is less than ideal, but it's a start!
    
    Please activate the functionality with '(load "lintian.el")' in your init.el.
    
    [1] https://gist.github.com/egmontkob/eb114294efbcd5adb1944c9f3cb5feda

---
+++ integration/emacs/lintian.el
@@ -0,0 +1,17 @@
+;; Thanks to bpalmer on Libera:#emacs for help with this module
+(require 'shr)
+(defcustom lintian-command "lintian" "*The path to the lintian executable")
+(defun lintian-run-file
+    (file) "Runs the lintian executable on the specified FILE."
+  (interactive "fFile: ")
+  (let
+      ((newbuf
+        (get-buffer-create "*packaging-hints*")))
+    (switch-to-buffer newbuf)
+    (erase-buffer)
+    (call-process lintian-command nil newbuf nil "--exp-output" "format=html" file)
+    (shr-render-region
+     (point-min)
+     (point-max))
+    (goto-char (point-min))))
+(provide 'lintian)
