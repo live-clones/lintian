live-clones/lintian

commit 3ad636e37a11dabe7063b66fbb1c8cd1321b53bb

Set teg preferences for the FTP Master Auto-Rejects directly from data; discard profile.
Gbp-Dch: ignore

lib/Lintian/Group.pm
@@ -205,17 +205,18 @@ sub process {
             say {*STDERR} encode_utf8('Loading overrides file (if any) ...')
               if $option->{debug};
 
-            my %alias = %{$self->profile->known_aliases};
             for my $override (@{$processable->overrides}) {
 
                 my $pattern = $override->pattern;
 
                 # catch renames
-                my $tag_name = $override->tag_name;
-                $tag_name = $alias{$tag_name}
-                  if length $alias{$tag_name};
+                my $tag_name
+                  = $self->profile->get_current_name($override->tag_name);
 
-                # also catches unknown tags
+                # catches unknown tags
+                next
+                  unless length $tag_name;
+
                 next
                   unless $self->profile->tag_is_enabled($tag_name);
 

bin/lintian
@@ -455,12 +455,6 @@ if ($selected{'packages-from-file'} && $#ARGV+1 > 0) {
     delete($selected{'packages-from-file'});
 }
 
-die encode_utf8("Cannot use profile together with --ftp-master-rejects.\n")
-  if $selected{profile} && $selected{'ftp-master-rejects'};
-# --ftp-master-rejects is implemented in a profile
-$selected{profile} = 'debian/ftp-master-auto-reject'
-  if $selected{'ftp-master-rejects'};
-
 @{$selected{'fail-on'}} = split(/,/, join($COMMA, @{$selected{'fail-on'}}));
 my @known_fail_on = qw(
   error
@@ -506,6 +500,30 @@ $PROFILE->load(
     !$command_line{'no-user-dirs'});
 say {*STDERR} encode_utf8('Using profile ' . $PROFILE->name . $DOT)
   if $selected{debug};
+
+if ($selected{'ftp-master-rejects'}) {
+    say {*STDERR}
+      encode_utf8(
+        'But only with tags enabled from the FTP Master Auto-Reject list.')
+      if $selected{debug};
+
+    my $rejection = $PROFILE->data->auto_rejection;
+
+    my @certain = @{$rejection->certain};
+    my @preventable = @{$rejection->preventable};
+
+    # disable all tags
+    $PROFILE->disable_tag($_) for $PROFILE->known_tags;
+
+    # enable the ones they want
+    $PROFILE->enable_tag($_) for (@certain, @preventable);
+
+    # no overrides allowed
+    $PROFILE->set_durable($_, 1) for @certain;
+
+    # overrides okay
+    $PROFILE->set_durable($_, 0) for @preventable;
+}
 
 my $envlc = List::Compare->new([keys %config], \@ENV_FROM_CONFIG);
 my @from_file = $envlc->get_intersection;

debian/rules
@@ -4,13 +4,10 @@ PERL ?= /usr/bin/perl
 
 VER := $(shell head -1 debian/changelog | sed -e 's/^.*(//' -e 's/).*$$//')
 tmp := $(CURDIR)/debian/lintian
-profiles := profiles/debian/main.profile \
-	    profiles/debian/ftp-master-auto-reject.profile
 pod2man := pod2man --center "Debian Package Checker" --release "Lintian v$(VER)"
 pod2mansources := $(wildcard man/*.pod)
 docsources := doc/lintian.rst README.md $(pod2mansources)
 perlprovides := data/fields/perl-provides
-autoreject_data := $(wildcard private/build-time-data/*)
 
 ifneq (,$(filter parallel=%,$(DEB_BUILD_OPTIONS)))
 	jobs = $(patsubst parallel=%,%,$(filter parallel=%,$(DEB_BUILD_OPTIONS)))
@@ -35,11 +32,6 @@ override_dh_install:
 	echo "Setting LINTIAN_VERSION to $(VER)"
 	$(PERL) -p -i -e 's/my \$$LINTIAN_VERSION;/my \$$LINTIAN_VERSION = q{$(VER)};/;' \
 	    $(tmp)/usr/share/lintian/bin/*
-
-profiles: $(profiles);
-
-$(profiles): $(autoreject_data) private/generate-profiles
-	private/generate-profiles
 
 api-doc:
 	private/generate-html-docs doc/api.html

lib/Lintian/Check/Debian/LintianOverrides/Duplicate.pm
@@ -36,17 +36,13 @@ with 'Lintian::Check';
 sub always {
     my ($self) = @_;
 
-    my %alias = %{$self->profile->known_aliases};
-
     my %pattern_tracker;
     for my $override (@{$self->processable->overrides}) {
 
         my $pattern = $override->pattern;
 
         # catch renames
-        my $tag_name = $override->tag_name;
-        $tag_name = $alias{$tag_name}
-          if length $alias{$tag_name};
+        my $tag_name = $self->profile->get_current_name($override->tag_name);
 
         push(@{$pattern_tracker{$tag_name}{$pattern}}, $override);
     }

lib/Lintian/Check/Debian/LintianOverrides/Mystery.pm
@@ -36,23 +36,21 @@ with 'Lintian::Check';
 sub always {
     my ($self) = @_;
 
-    my %alias = %{$self->profile->known_aliases};
-
     for my $override (@{$self->processable->overrides}) {
-
-        my $tag_name = $override->tag_name;
-        next
-          if defined $self->profile->get_tag($tag_name);
 
         my $override_item = $self->processable->override_file;
         my $pointer = $override_item->pointer($override->position);
 
-        $self->pointed_hint('alien-tag', $pointer, $tag_name)
-          if !length $alias{$tag_name};
+        my $mystery_name = $override->tag_name;
+        my $current_name = $self->profile->get_current_name($mystery_name);
 
-        $self->pointed_hint('renamed-tag', $pointer, $tag_name, $ARROW,
-            $alias{$tag_name})
-          if length $alias{$tag_name};
+        $self->pointed_hint('alien-tag', $pointer, $mystery_name)
+          if !length $current_name;
+
+        $self->pointed_hint('renamed-tag', $pointer, $mystery_name, $ARROW,
+            $current_name)
+          if length $current_name
+          && $current_name ne $mystery_name;
     }
 
     return;

lib/Lintian/Profile.pm
@@ -24,7 +24,7 @@ use v5.20;
 use warnings;
 use utf8;
 
-use Carp qw(croak);
+use Carp qw(carp croak);
 use Const::Fast;
 use Cwd qw(realpath);
 use File::BaseDir qw(config_home config_files data_home);
@@ -317,7 +317,7 @@ sub load {
 
             # record known aliases
             my @taken
-              = grep { defined $self->known_aliases->{$_} }
+              = grep { exists $self->known_aliases->{$_} }
               @{$tag->renamed_from};
 
             die encode_utf8('These aliases of the tag '
@@ -326,7 +326,20 @@ sub load {
                   . join($SPACE, @taken))
               if @taken;
 
-            $self->known_aliases->{$_} = $tag->name for @{$tag->renamed_from};
+            for my $old_name (@{$tag->renamed_from}) {
+
+                if (exists $self->known_aliases->{$old_name}) {
+
+                    my $taken = $self->known_aliases->{$old_name};
+                    my $tag_name = $tag->name;
+                    warn encode_utf8(
+"Alias $old_name for $tag_name ignored; already taken by $taken"
+                    );
+
+                } else {
+                    $self->known_aliases->{$old_name} = $tag->name;
+                }
+            }
         }
     }
 
@@ -397,9 +410,53 @@ Otherwise it returns undef.
 =cut
 
 sub get_tag {
-    my ($self, $name) = @_;
+    my ($self, $maybe_historical) = @_;
 
-    return $self->known_tags_by_name->{$name};
+    my $name = $self->get_current_name($maybe_historical);
+    return undef
+      unless length $name;
+
+    return $self->known_tags_by_name->{$name}
+      if exists $self->known_tags_by_name->{$name};
+
+    return undef;
+}
+
+=item get_current_name
+
+=cut
+
+sub get_current_name {
+    my ($self, $tag_name) = @_;
+
+    return $self->known_aliases->{$tag_name}
+      if exists $self->known_aliases->{$tag_name};
+
+    return $tag_name
+      if exists $self->known_tags_by_name->{$tag_name};
+
+    return $EMPTY;
+}
+
+=item set_durable ($tag)
+
+=cut
+
+sub set_durable {
+    my ($self, $maybe_historical, $status) = @_;
+
+    my $tag = $self->get_tag($maybe_historical);
+    croak encode_utf8("Unknown tag $maybe_historical.")
+      unless defined $tag;
+
+    $self->durable_tags_by_name->{$tag->name} = 1
+      if $status;
+
+    # settings from tag govern
+    delete $self->durable_tags_by_name->{$tag->name}
+      if !$status && !$tag->show_always;
+
+    return;
 }
 
 =item $prof->is_durable ($tag)
@@ -410,9 +467,17 @@ Returns a false value if the tag has been marked as
 =cut
 
 sub is_durable {
-    my ($self, $tag_name) = @_;
+    my ($self, $maybe_historical) = @_;
 
-    return exists $self->durable_tags_by_name->{$tag_name};
+    my $tag = $self->get_tag($maybe_historical);
+    croak encode_utf8("Unknown tag $maybe_historical.")
+      unless defined $tag;
+
+    return 1
+      if $tag->show_always
+      || exists $self->durable_tags_by_name->{$tag->name};
+
+    return 0;
 }
 
 =item $prof->known_checks
@@ -442,25 +507,16 @@ Enables a tag.
 =cut
 
 sub enable_tag {
-    my ($self, $name) = @_;
+    my ($self, $maybe_historical) = @_;
 
-    my $renamed = $self->known_aliases->{$name};
-    if (length $renamed) {
-
-        warn encode_utf8(
-"The tag $name was renamed to $renamed. Please adjust your profile.\n"
-        );
-        $name = $renamed;
-    }
-
-    my $tag = $self->known_tags_by_name->{$name};
-    die encode_utf8("Unknown tag $name")
-      unless $tag;
+    my $tag = $self->get_tag($maybe_historical);
+    croak encode_utf8("Unknown tag $maybe_historical.")
+      unless defined $tag;
 
     $self->enabled_checks_by_name->{$tag->check}++
-      unless exists $self->enabled_tags_by_name->{$name};
+      unless exists $self->enabled_tags_by_name->{$tag->name};
 
-    $self->enabled_tags_by_name->{$name} = 1;
+    $self->enabled_tags_by_name->{$tag->name} = 1;
 
     return;
 }
@@ -472,26 +528,17 @@ Disable a tag.
 =cut
 
 sub disable_tag {
-    my ($self, $name) = @_;
+    my ($self, $maybe_historical) = @_;
 
-    my $renamed = $self->known_aliases->{$name};
-    if (length $renamed) {
-
-        warn encode_utf8(
-"The tag $name was renamed to $renamed. Please adjust your profile.\n"
-        );
-        $name = $renamed;
-    }
-
-    my $tag = $self->known_tags_by_name->{$name};
-    die encode_utf8("Unknown tag $name")
-      unless $tag;
+    my $tag = $self->get_tag($maybe_historical);
+    croak encode_utf8("Unknown tag $maybe_historical.")
+      unless defined $tag;
 
     delete $self->enabled_checks_by_name->{$tag->check}
-      unless exists $self->enabled_tags_by_name->{$name}
+      unless exists $self->enabled_tags_by_name->{$tag->name}
       && --$self->enabled_checks_by_name->{$tag->check};
 
-    delete $self->enabled_tags_by_name->{$name};
+    delete $self->enabled_tags_by_name->{$tag->name};
 
     return;
 }
@@ -721,10 +768,14 @@ sub display_level_for_tag {
 =cut
 
 sub tag_is_enabled {
-    my ($self, $tag_name) = @_;
+    my ($self, $maybe_historical) = @_;
+
+    my $tag = $self->get_tag($maybe_historical);
+    croak encode_utf8("Unknown tag $maybe_historical.")
+      unless defined $tag;
 
     return 1
-      if exists $self->enabled_tags_by_name->{$tag_name};
+      if exists $self->enabled_tags_by_name->{$tag->name};
 
     return 0;
 }

private/auto-reject-diff
@@ -1,0 +1,166 @@
+#!/usr/bin/perl
+#
+# Copyright © 2021 Felix Lechner
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, you can find it on the World Wide
+# Web at http://www.gnu.org/copyleft/gpl.html, or write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+use v5.20;
+use warnings;
+use utf8;
+
+use Cwd qw(realpath);
+use File::Basename qw(dirname);
+
+# neither Path::This nor lib::relative are in Debian
+use constant THISFILE => realpath __FILE__;
+use constant THISDIR => dirname realpath __FILE__;
+
+# use Lintian modules that belong to this program
+use lib THISDIR . '/../lib';
+
+use Const::Fast;
+use List::Compare;
+use List::Util qw(uniq);
+use Unicode::UTF8 qw(encode_utf8);
+
+use Lintian::Archive;
+use Lintian::Profile;
+
+const my $SPACE => q{ };
+const my $INDENT => $SPACE x 4;
+const my $HYPHEN => q{-};
+
+$ENV{LINTIAN_BASE} = realpath(THISDIR . '/..')
+  // die encode_utf8('Cannot resolve LINTIAN_BASE');
+
+refresh_auto_reject_data();
+
+# reload
+my $profile = Lintian::Profile->new;
+$profile->load;
+
+my $data = $profile->data;
+
+my @want_certain = @{$data->auto_rejection->certain};
+my @want_preventable = @{$data->auto_rejection->preventable};
+
+# find all tags known to Lintian
+my @known_tags = $profile->known_tags;
+my %new_name;
+
+for my $tag_name (@known_tags) {
+
+    my $tag = $profile->get_tag($tag_name);
+
+    my @renamed_from = @{$tag->renamed_from};
+
+    my @taken = grep { exists $new_name{$_} } @renamed_from;
+
+    say encode_utf8(
+"Warning: Ignoring $_ as an alias for $new_name{$_} in favor of $tag_name."
+    )for @taken;
+
+    $new_name{$_} = $tag_name for @renamed_from;
+}
+
+my $old_certain_lc= List::Compare->new(\@want_certain, [keys %new_name]);
+my $old_preventable_lc
+  = List::Compare->new(\@want_preventable, [keys %new_name]);
+
+my @old_certain_names = $old_certain_lc->get_intersection;
+my @old_preventable_names = $old_preventable_lc->get_intersection;
+
+say encode_utf8('FTP Master uses those old tag names for auto-rejection:')
+  if @old_certain_names || @old_preventable_names;
+say encode_utf8($INDENT . "-   [certain]   $_  =>  $new_name{$_}")
+  for @old_certain_names;
+say encode_utf8($INDENT . "- [preventable] $_  =>  $new_name{$_}")
+  for @old_preventable_names;
+
+my $new_certain_lc
+  = List::Compare->new(\@want_certain,
+    [map { $new_name{$_} } @old_certain_names]);
+my $new_preventable_lc
+  = List::Compare->new(\@want_preventable,
+    [map { $new_name{$_} } @old_preventable_names]);
+
+my @aware_certain_names = $new_certain_lc->get_intersection;
+my @aware_preventable_names = $new_preventable_lc->get_intersection;
+
+say encode_utf8('They already know about those tags:')
+  if @aware_certain_names || @aware_preventable_names;
+say encode_utf8($INDENT . "-   [certain]   $_") for @aware_certain_names;
+say encode_utf8($INDENT . "- [preventable] $_") for @aware_preventable_names;
+
+my @unaware_certain_names = $new_certain_lc->get_Ronly;
+my @unaware_preventable_names = $new_preventable_lc->get_Ronly;
+
+say encode_utf8('The following tags have to be added:')
+  if @unaware_certain_names || @unaware_preventable_names;
+say encode_utf8($INDENT . "-   [certain]   $_") for @unaware_certain_names;
+say encode_utf8($INDENT . "- [preventable] $_") for @unaware_preventable_names;
+
+# replace old names
+@want_certain = uniq map { $new_name{$_} // $_ } @want_certain;
+@want_preventable = uniq map { $new_name{$_} // $_ } @want_preventable;
+
+my $certain_lc = List::Compare->new(\@want_certain, \@known_tags);
+my @unknown_certain = $certain_lc->get_Lonly;
+my @certain = $certain_lc->get_intersection;
+
+my $preventable_lc = List::Compare->new(\@want_preventable, \@known_tags);
+my @unknown_preventable = $preventable_lc->get_Lonly;
+my @preventable = $preventable_lc->get_intersection;
+my @unknown = (@unknown_certain, @unknown_preventable);
+
+say encode_utf8(
+    'Warning, disregarding unknown tags for FTP Master Auto-Rejects:')
+  if @unknown;
+say encode_utf8($INDENT . $HYPHEN . $SPACE . $_) for @unknown;
+
+say encode_utf8('Found '
+      . scalar @certain
+      . ' certain and '
+      . scalar @preventable
+      . ' preventable tags for FTP Master Auto-Rejects.');
+
+exit 0;
+
+sub refresh_auto_reject_data {
+
+    my $refresh_profile = Lintian::Profile->new;
+    $refresh_profile->load;
+
+    my $refresh_data = $refresh_profile->data;
+
+    my $archive = Lintian::Archive->new;
+    my $basedir = "$ENV{LINTIAN_BASE}/data";
+
+    # refresh data
+    $refresh_data->auto_rejection->refresh($archive, $basedir);
+
+    undef $refresh_profile;
+    undef $refresh_data;
+
+    return;
+}
+
+# Local Variables:
+# indent-tabs-mode: nil
+# cperl-indent-level: 4
+# End:
+# vim: syntax=perl sw=4 sts=4 sr et

private/generate-profiles
@@ -1,215 +1,0 @@
-#!/usr/bin/perl
-
-# Generates profiles for the Debian vendor
-#  - Remember to add new profiles to d/rules under profiles
-
-use v5.20;
-use warnings;
-use utf8;
-
-use Cwd qw(realpath);
-use File::Basename qw(dirname);
-
-# neither Path::This nor lib::relative are in Debian
-use constant THISFILE => realpath __FILE__;
-use constant THISDIR => dirname realpath __FILE__;
-
-# use Lintian modules that belong to this program
-use lib THISDIR . '/../lib';
-
-use Const::Fast;
-use File::Find::Rule;
-use List::Compare;
-use List::Util qw(uniq);
-use Path::Tiny;
-use Unicode::UTF8 qw(encode_utf8);
-
-use Lintian::Archive;
-use Lintian::Deb822;
-use Lintian::Profile;
-
-const my $EMPTY => q{};
-const my $SPACE => q{ };
-const my $INDENT => $SPACE x 4;
-const my $NEWLINE => qq{\n};
-const my $COMMA => q{,};
-const my $HYPHEN => q{-};
-
-$ENV{LINTIAN_BASE} = realpath(THISDIR . '/..')
-  // die encode_utf8('Cannot resolve LINTIAN_BASE');
-
-refresh_data();
-
-# reload
-my $profile = Lintian::Profile->new;
-$profile->load;
-
-my $data = $profile->data;
-
-my @want_fatal = @{$data->auto_rejection->certain};
-my @want_nonfatal = @{$data->auto_rejection->preventable};
-
-# find all tags known to Lintian
-my @known_tags;
-my %new_name;
-my $tagroot = "$ENV{LINTIAN_BASE}/tags";
-my @descfiles = File::Find::Rule->file()->name('*.tag')->in($tagroot);
-for my $tagpath (@descfiles) {
-
-    my $deb822 = Lintian::Deb822->new;
-    my @sections = $deb822->read_file($tagpath);
-    die encode_utf8("Tag in $tagpath does not have at least one paragraph")
-      unless @sections;
-
-    my $fields = $sections[0];
-
-    my $name = $fields->value('Tag');
-    push(@known_tags, $name);
-
-    my @renamed_from = $fields->trimmed_list('Renamed-From');
-
-    my @taken = grep { exists $new_name{$_} } @renamed_from;
-
-    say encode_utf8(
-        "Warning: Ignoring $_ as an alias for $new_name{$_} in favor of $name."
-    )for @taken;
-
-    $new_name{$_} = $name for @renamed_from;
-}
-
-my $old_fatal_lc= List::Compare->new(\@want_fatal, [keys %new_name]);
-my $old_nonfatal_lc= List::Compare->new(\@want_nonfatal, [keys %new_name]);
-
-my @old_fatal_names = $old_fatal_lc->get_intersection;
-my @old_nonfatal_names = $old_nonfatal_lc->get_intersection;
-
-say encode_utf8('FTP Master uses old tag names for auto-rejection:')
-  if @old_fatal_names || @old_nonfatal_names;
-say encode_utf8($INDENT . "-   [fatal]   $_  =>  $new_name{$_}")
-  for @old_fatal_names;
-say encode_utf8($INDENT . "- [non-fatal] $_  =>  $new_name{$_}")
-  for @old_nonfatal_names;
-
-my $new_fatal_lc
-  = List::Compare->new(\@want_fatal, [map { $new_name{$_} } @old_fatal_names]);
-my $new_nonfatal_lc
-  = List::Compare->new(\@want_nonfatal,
-    [map { $new_name{$_} } @old_nonfatal_names]);
-
-my @aware_fatal_names = $new_fatal_lc->get_intersection;
-my @aware_nonfatal_names = $new_nonfatal_lc->get_intersection;
-
-say encode_utf8('They already know about those tags:')
-  if @aware_fatal_names || @aware_nonfatal_names;
-say encode_utf8($INDENT . "-   [fatal]   $_") for @aware_fatal_names;
-say encode_utf8($INDENT . "- [non-fatal] $_") for @aware_nonfatal_names;
-
-my @unaware_fatal_names = $new_fatal_lc->get_Ronly;
-my @unaware_nonfatal_names = $new_nonfatal_lc->get_Ronly;
-
-say encode_utf8('The following tags have to be added:')
-  if @unaware_fatal_names || @unaware_nonfatal_names;
-say encode_utf8($INDENT . "-   [fatal]   $_") for @unaware_fatal_names;
-say encode_utf8($INDENT . "- [non-fatal] $_") for @unaware_nonfatal_names;
-
-# replace old names
-@want_fatal = uniq map { $new_name{$_} // $_ } @want_fatal;
-@want_nonfatal = uniq map { $new_name{$_} // $_ } @want_nonfatal;
-
-my $fatal_lc = List::Compare->new(\@want_fatal, \@known_tags);
-my @unknown_fatal = $fatal_lc->get_Lonly;
-my @fatal = $fatal_lc->get_intersection;
-
-my $nonfatal_lc = List::Compare->new(\@want_nonfatal, \@known_tags);
-my @unknown_nonfatal = $nonfatal_lc->get_Lonly;
-my @nonfatal = $nonfatal_lc->get_intersection;
-my @unknown = (@unknown_fatal, @unknown_nonfatal);
-
-say encode_utf8(
-    'Warning, disregarding unknown tags for profile ftp-master-auto-reject:')
-  if @unknown;
-say encode_utf8($INDENT . $HYPHEN . $SPACE . $_) for @unknown;
-
-say encode_utf8('Found '
-      . scalar @fatal
-      . ' fatal and '
-      . scalar @nonfatal
-      . ' non-fatal tags for profile ftp-master-auto-reject.');
-
-generate_profile(
-    'debian',
-    'ftp-master-auto-reject',
-    {
-        # "lintian" is enabled by default, so we explicitly disable it.
-        'Disable-Tags-From-Check' => ['lintian'],
-        'Enable-Tags' => [@fatal, @nonfatal],
-    },
-    {
-        'Tags' => \@fatal,
-        'Overridable' => ['no'],
-    });
-
-exit 0;
-
-sub generate_profile {
-    my ($vendor, $name, @paragraphs) = @_;
-
-    my $text =<<"EOSTR";
-# This profile is auto-generated
-Profile: $vendor/$name
-EOSTR
-
-    $text .= write_paragraph($_) for @paragraphs;
-
-    my $folder = "profiles/$vendor";
-    path($folder)->mkpath
-      unless -d $folder;
-
-    path("$folder/$name.profile")->spew_utf8($text);
-
-    return;
-}
-
-sub write_paragraph {
-    my ($paragraph) = @_;
-
-    my $text = $EMPTY;
-
-    for my $field (sort keys %{$paragraph}) {
-        $text .= "$field:" . $NEWLINE;
-
-        my @values = sort @{$paragraph->{$field}};
-
-        $text .= $SPACE . $_ . $NEWLINE for @values;
-    }
-
-    $text .= $NEWLINE
-      if length $text;
-
-    return $text;
-}
-
-sub refresh_data {
-
-    my $refresh_profile = Lintian::Profile->new;
-    $refresh_profile->load;
-
-    my $refresh_data = $refresh_profile->data;
-
-    my $archive = Lintian::Archive->new;
-    my $basedir = "$ENV{LINTIAN_BASE}/data";
-
-    # refresh data
-    $refresh_data->auto_rejection->refresh($archive, $basedir);
-
-    undef $refresh_profile;
-    undef $refresh_data;
-
-    return;
-}
-
-# Local Variables:
-# indent-tabs-mode: nil
-# cperl-indent-level: 4
-# End:
-# vim: syntax=perl sw=4 sts=4 sr et

commit 53f0243677be9b1e3ad25157d5db8402479ae074

Prevent issuance of tags under their old names.
Julian Gilbey was able to spot [1] a super obscure bug introduced recently when
the handling of tags was revamped so that old tag names could be shown on the
website along with their current names.

[1] https://salsa.debian.org/lintian/lintian/-/commit/5e20ecb49725912c7d7a42a721a38987981fd0dd#note_291378

The bug was probably introduced in commit 3ad636e37a1.

Lintian now complains, as it should, about the old tag names being issued with
the two warnings below. This commit also fixes those warnings.

Warning in processable [...]/requests-undeclared-python-versions_1.0.dsc:
 Tried to issue renamed tag declare-requested-python-versions-for-test
 (current name declare-python-versions-for-test)
 in check testsuite. at [...]/lib/Lintian/Group.pm line 333.

Warning in processable [...]/does-not-query-declared-python-versions_1.0.dsc:
 Tried to issue renamed tag query-requested-python-versions-in-test
 (current name query-declared-python-versions-in-test)
 in check testsuite. at [...]/lib/Lintian/Group.pm line 333.

Thanks to Julian for his extraordinary attention!

Gbp-Dch: ignore

lib/Lintian/Check/Testsuite.pm
@@ -309,13 +309,13 @@ sub check_test_file {
 
             my $debian_control = $self->processable->debian_control;
 
-            $self->pointed_hint('declare-requested-python-versions-for-test',
+            $self->pointed_hint('declare-python-versions-for-test',
                 $pointer, $command)
               if $options =~ m{ \s (?: -\w*r | --requested ) }x
               && !$debian_control->source_fields->declares(
                 'X-Python3-Version');
 
-            $self->pointed_hint('query-requested-python-versions-in-test',
+            $self->pointed_hint('query-declared-python-versions-in-test',
                 $pointer,
                 $debian_control->source_fields->value('X-Python3-Version'))
               if $options =~ m{ \s (?: -\w*s | --supported ) }x

lib/Lintian/Group.pm
@@ -304,39 +304,50 @@ sub process {
 
         for my $hint (@from_checks) {
 
-            my $tag_name = $hint->tag_name;
+            my $as_issued = $hint->tag_name;
 
             croak encode_utf8('No tag name')
-              unless length $tag_name;
+              unless length $as_issued;
 
             my $issuer = $hint->issued_by;
 
             # try local name space
-            my $tag = $self->profile->get_tag("$issuer/$tag_name");
+            my $tag = $self->profile->get_tag("$issuer/$as_issued");
 
             warn encode_utf8(
-"Using tag $tag_name as name spaced while not so declared (in check $issuer)."
+"Using tag $as_issued as name spaced while not so declared (in check $issuer)."
             )if defined $tag && !$tag->name_spaced;
 
             # try global name space
-            $tag ||= $self->profile->get_tag($tag_name);
+            $tag ||= $self->profile->get_tag($as_issued);
 
             unless (defined $tag) {
                 warn encode_utf8(
-                    "Tried to issue unknown tag $tag_name in check $issuer.");
+                    "Tried to issue unknown tag $as_issued in check $issuer.");
+                next;
+            }
+
+            if (  !$tag->name_spaced && $tag->name ne $as_issued
+                || $tag->name_spaced && $tag->name ne "$issuer/$as_issued") {
+
+                my $current_name = $tag->name;
+                warn encode_utf8(
+"Tried to issue renamed tag $as_issued (current name $current_name) in check $issuer."
+                );
+
                 next;
             }
 
             my $owner = $tag->check;
             if ($issuer ne $owner) {
                 warn encode_utf8(
-                    "Check $issuer has no tag $tag_name (but $owner does).");
+                    "Check $issuer has no tag $as_issued (but $owner does).");
                 next;
             }
 
             # pull name from tag; could be name-spaced
             $hint->tag_name($tag->name);
-            $tag_name = $hint->tag_name;
+            my $tag_name = $hint->tag_name;
 
             # skip disabled tags
             next

tags/q/query-declared-python-versions-in-test.tag
@@ -1,4 +1,4 @@
-Tag:  query-declared-python-versions-in-test
+Tag: query-declared-python-versions-in-test
 Severity: warning
 Check: testsuite
 Renamed-From: