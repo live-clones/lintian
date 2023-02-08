aad82662b72dce885fea4d809ff07f8850ee8e8e
Christopher Obbard <chris.obbard@collabora.com>
2023-02-08 15:33:21 +0000

    Correctly determine if package has been built with golang
    Golang packages may be built statically-linked (for instance, if the
    package does not use any system libraries) and currently the
    statically-linked-binary check generates lots of false-positives due to
    not detecting statically-linked golang packages correctly.
    
    Currently the statically-linked-binary check has two checks:
    
      1) The binary package name begins with "golang-", which is not enough
         for packages which ship a golang binary (e.g. debos & fakemachine).
      2) Check the Build-Depends-All field for a golang compiler, which isn't
         available in binary packages.
    
    Add some additional sanity checks to really tell if binary packages are
    built with a golang compiler:
    
      1) Check the package Section field for golang.
      2) Check the binary package was built with a golang compiler by checking
         the Built-Using and Static-Built-Using fields.
      3) Check the source package name (i.e. the name of the source package
         which the binary was built from) starts with "golang-".
    
    Signed-off-by: Christopher Obbard <chris.obbard@collabora.com>

--- lib/Lintian/Check/Binaries/Static.pm
+++ lib/Lintian/Check/Binaries/Static.pm
@@ -38,16 +38,34 @@ has built_with_golang => (
     default => sub {
         my ($self) = @_;
 
-        my $built_with_golang = $self->processable->name =~ m/^golang-/;
+        # Check source package name starts with "golang-"
+        if ($self->processable->source_name =~ m/^golang-/) {
+          return 1;
+        }
 
-        my $source = $self->group->source;
+        # Check package section is golang
+        if ($self->processable->fields->value('Section') eq 'golang') {
+          return 1;
+        }
 
-        $built_with_golang
-          = $source->relation('Build-Depends-All')
-          ->satisfies('golang-go | golang-any')
-          if defined $source;
+        # Check binary package was built using golang
+        if ($self->processable->fields->value('Built-Using') =~ m/golang-\d\.\d+/ ||
+            $self->processable->fields->value('Static-Built-Using') =~ m/golang-\d\.\d+/) {
+          return 1;
+        }
 
-        return $built_with_golang;
+        # Check binary package name starts with "golang-"
+        if ($self->processable->name =~ m/^golang-/) {
+          return 1;
+        }
+
+        # Check source package build-depends contains a golang compiler
+        if (defined($self->group->source)) {
+          return $self->group->source->relation('Build-Depends-All')
+            ->satisfies('golang-go | golang-any');
+        }
+
+        return 0;
     }
 );
 
