d7b05083da0115797ef16c7fc166b87f63fd6f89
Rebecca N. Palmer <rebecca_palmer@zoho.com>
2024-09-26 12:48:47 +0000

    Improve uses-deprecated-python-stdlib
    Closes: #1077324

--- t/recipes/checks/languages/python/stdlib-deprecation/python-imports-asyncore/build-spec/orig/import.py
+++ t/recipes/checks/languages/python/stdlib-deprecation/python-imports-asyncore/build-spec/orig/import.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 import asyncore
+import asyncore, struct
 import struct, socket, argparse, asyncore
 import struct, asyncore, argparse
+import asyncore as ac
+import struct,asyncore,argparse
