e1fc2908ad28ffe47d9573114c98ea32c31a3f38
Maytham Alsudany <maytha8thedev@gmail.com>
2025-01-22 20:58:55 +0800

    Replace reference to non-existent "unpack_errors" property with "unpack_messages"
    Closes: #1065175

--- lib/Lintian/Processable/Source/Orig.pm
+++ lib/Lintian/Processable/Source/Orig.pm
@@ -161,7 +161,7 @@ has orig => (
                         # shortens paths; keeps same base directory
                         my $sub_errors = $subindex->drop_common_prefix;
 
-                        push(@{$index->unpack_errors}, "$tarball . $_")
+                        push(@{$index->unpack_messages}, "$tarball . $_")
                           for uniq split(/\n/, $sub_errors);
                     }
                 }
