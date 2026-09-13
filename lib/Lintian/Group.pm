live-clones/lintian

commit 697de993f07145e9f4c00aaec3a87514c4bca29c

Always send all debugging and status information to STDERR.
Greatly simplifies the handling of file descriptors in Lintian.

lib/Lintian/Group.pm
@@ -217,12 +217,15 @@ sub process {
 
         my $declared_overrides;
 
-        $OUTPUT->debug_msg(1,
-            'Base directory for processable: ' . $processable->basedir);
+        say {*STDERR}
+          encode_utf8(
+            'Base directory for processable: '. $processable->basedir)
+          if $option->{debug} >= 1;
 
         unless ($option->{'no-override'}) {
 
-            $OUTPUT->debug_msg(1, 'Loading overrides file (if any) ...');
+            say {*STDERR} encode_utf8('Loading overrides file (if any) ...')
+              if $option->{debug} >= 1;
 
             eval {$declared_overrides = $processable->overrides;};
             if (my $err = $@) {
@@ -305,7 +308,8 @@ sub process {
 
             my $timer = [gettimeofday];
             my $procid = $processable->identifier;
-            $OUTPUT->debug_msg(1, "Running check: $name on $procid  ...");
+            say {*STDERR} encode_utf8("Running check: $name on $procid  ...")
+              if $option->{debug} >= 1;
 
             my $absolute = $self->profile->check_path_by_name->{$name};
             require $absolute;
@@ -335,8 +339,10 @@ sub process {
             }
 
             my $tres = sprintf('%.3fs', $raw_res);
-            $OUTPUT->debug_msg(1,"Check $name for $procid done ($tres)");
-            $OUTPUT->perf_log("$procid,check/$name,${raw_res}");
+            say {*STDERR} encode_utf8("Check $name for $procid done ($tres)")
+              if $option->{debug} >= 1;
+            say {*STDERR} encode_utf8("$procid,check/$name,$raw_res")
+              if $option->{'perf-output'};
         }
 
         my %used_overrides;
@@ -417,9 +423,11 @@ sub process {
 
     my $raw_res = tv_interval($group_timer);
     my $tres = sprintf('%.3fs', $raw_res);
-    $OUTPUT->debug_msg(1,
-        'Checking all of group ' . $self->name . " done ($tres)");
-    $OUTPUT->perf_log($self->name . ",total-group-check,${raw_res}");
+    say {*STDERR}
+      encode_utf8('Checking all of group ' . $self->name . " done ($tres)")
+      if $option->{debug} >= 1;
+    say {*STDERR} encode_utf8($self->name . ",total-group-check,$raw_res")
+      if $option->{'perf-output'};
 
     if ($option->{'debug'} > 2) {
 
@@ -430,17 +438,20 @@ sub process {
         my $group_id = $pivot->source . $SLASH . $pivot->source_version;
         my $group_usage
           = human_bytes(total_size([map { $_ } $self->get_processables]));
-        $OUTPUT->debug_msg($EXTRA_VERBOSE,
-            "Memory usage [group:$group_id]: $group_usage");
+        say {*STDERR}
+          encode_utf8("Memory usage [group:$group_id]: $group_usage")
+          if $option->{debug} >= $EXTRA_VERBOSE;
 
         for my $processable ($self->get_processables) {
             my $id = $processable->identifier;
             my $usage = human_bytes(total_size($processable));
-            $OUTPUT->debug_msg($EXTRA_VERBOSE, "Memory usage [$id]: $usage");
+
+            say {*STDERR} encode_utf8("Memory usage [$id]: $usage")
+              if $option->{debug} >= $EXTRA_VERBOSE;
         }
     }
 
-    $self->clean_lab($OUTPUT);
+    $self->clean_lab($option);
 
     return $success;
 }
@@ -453,28 +464,35 @@ also get these unless we are keeping the lab.
 =cut
 
 sub clean_lab {
-    my ($self, $OUTPUT) = @_;
+    my ($self, $option) = @_;
 
     my $total = [gettimeofday];
 
     for my $processable ($self->get_processables) {
 
         my $proc_id = $processable->identifier;
-        $OUTPUT->debug_msg(1, "Auto removing: $proc_id ...");
+        say {*STDERR} encode_utf8("Auto removing: $proc_id ...")
+          if $option->{debug} >= 1;
+
         my $each = [gettimeofday];
 
         $processable->remove;
 
         my $raw_res = tv_interval($each);
-        $OUTPUT->debug_msg(1, "Auto removing: $proc_id done (${raw_res}s)");
-        $OUTPUT->perf_log("$proc_id,auto-remove entry,$raw_res");
+        say {*STDERR} encode_utf8("Auto removing: $proc_id done (${raw_res}s)")
+          if $option->{debug} >= 1;
+        say {*STDERR} encode_utf8("$proc_id,auto-remove entry,$raw_res")
+          if $option->{'perf-output'};
     }
 
     my $raw_res = tv_interval($total);
     my $tres = sprintf('%.3fs', $raw_res);
-    $OUTPUT->debug_msg(1,
-        'Auto-removal all for group ' . $self->name . " done ($tres)");
-    $OUTPUT->perf_log($self->name . ",total-group-auto-remove,$raw_res");
+    say {*STDERR}
+      encode_utf8(
+        'Auto-removal all for group ' . $self->name . " done ($tres)")
+      if $option->{debug} >= 1;
+    say {*STDERR}encode_utf8($self->name . ",total-group-auto-remove,$raw_res")
+      if $option->{'perf-output'};
 
     return;
 }
@@ -495,7 +513,7 @@ sub add_processable{
     my ($self, $processable) = @_;
 
     if ($processable->tainted) {
-        warn(
+        warn encode_utf8(
             sprintf(
                 "warning: tainted %1\$s package '%2\$s', skipping\n",
                 $processable->type, $processable->name

bin/lintian
@@ -151,7 +151,6 @@ my @getoptions = qw(
   packages-from-file=s
   pedantic
   perf-debug
-  perf-output=s
   print-version
   profile=s
   quiet|q
@@ -423,14 +422,11 @@ if (length $selected{'suppress-tags-from-file'}) {
     }
 }
 
-my @CLOSE_AT_END;
-my $received_signal;
 my $exit_code = 0;
-my $STATUS_FD;
 
 # root permissions?
 # check if effective UID is 0
-warn encode_utf8("warning: running with root privileges is not recommended!\n")
+warn encode_utf8("running with root privileges is not recommended!\n")
   if $> == 0 && !$selected{'allow-root'};
 
 if ($selected{'ignore-lintian-env'}) {
@@ -440,7 +436,7 @@ if ($selected{'ignore-lintian-env'}) {
 # option --all and packages specified at the same time?
 if ($selected{'packages-from-file'} && $#ARGV+1 > 0) {
     warn encode_utf8(
-"warning: option --packages-from-file cannot be mixed with package parameters!\n"
+"option --packages-from-file cannot be mixed with package parameters!\n"
     );
     warn encode_utf8("(will ignore --packages-from-file option)\n");
 
@@ -524,10 +520,12 @@ warn encode_utf8(
   if @already;
 
 my @not_yet = grep { !defined $ENV{$_} } @from_file;
-$OUTPUT->debug_msg(1,
-    'Setting environment variables from configuration file: '
-      . join($SPACE, @not_yet))
-  if @not_yet;
+if (@not_yet) {
+    say {*STDERR}
+      encode_utf8('Setting environment variables from configuration file: '
+          . join($SPACE, @not_yet))
+      if $selected{debug} >= 1;
+}
 $ENV{$_} = $config{$_} for @not_yet;
 
 die encode_utf8("The color value must be one of auto, always, or never.\n")
@@ -559,7 +557,6 @@ if ($selected{debug}) {
 }
 
 $OUTPUT->verbosity($selected{verbose});
-$OUTPUT->debug($selected{debug});
 
 $OUTPUT->color(1)
   if $selected{color} eq 'always'
@@ -569,23 +566,6 @@ $OUTPUT->color(1)
 $OUTPUT->tty_hyperlinks($hyperlinks_capable&& $selected{hyperlinks} eq 'on');
 $OUTPUT->tag_display_limit($selected{'tag-display-limit'});
 $OUTPUT->showdescription($selected{info});
-
-$OUTPUT->perf_debug($selected{'perf-debug'});
-if (defined(my $perf_log = $selected{'perf-output'})) {
-    my $fd = open_file_or_fd($perf_log, '>');
-    $OUTPUT->perf_log_fd($fd);
-
-    push(@CLOSE_AT_END, [$fd, $perf_log]);
-}
-
-if (defined(my $status_log = $selected{'status-log'})) {
-    $STATUS_FD = open_file_or_fd($status_log, '>');
-    $STATUS_FD->autoflush;
-
-    push(@CLOSE_AT_END, [$STATUS_FD, $status_log]);
-} else {
-    open($STATUS_FD, '>', '/dev/null');
-}
 
 # check for arguments
 unless (@ARGV || $selected{'packages-from-file'}) {
@@ -604,22 +584,16 @@ unless (@ARGV || $selected{'packages-from-file'}) {
       unless $ok;
 }
 
-if ($selected{debug}) {
-    # Lintian::Output is now available
-    $OUTPUT->debug_msg(
-        1,
-        "Lintian v$ENV{LINTIAN_VERSION}",
-        "Lintian root directory: $ENV{LINTIAN_BASE}",
-        "Configuration file: $LINTIAN_CFG",
-        'UTF-8: ✓ (☃)',
-        $OUTPUT->delimiter,
-    );
+if ($selected{debug} >= 1) {
+    say {*STDERR} encode_utf8("Lintian v$ENV{LINTIAN_VERSION}");
+    say {*STDERR} encode_utf8("Lintian root directory: $ENV{LINTIAN_BASE}");
+    say {*STDERR} encode_utf8("Configuration file: $LINTIAN_CFG");
+    say {*STDERR} encode_utf8('UTF-8: ✓ (☃)');
 }
 
 if (defined $selected{LINTIAN_PROFILE}) {
     warn encode_utf8(
-"Warning: Please use 'profile' in config file; LINTIAN_PROFILE is obsolete.\n"
-    );
+        "Please use 'profile' in config file; LINTIAN_PROFILE is obsolete.\n");
     $selected{profile} //= $selected{LINTIAN_PROFILE};
     delete $selected{LINTIAN_PROFILE};
 }
@@ -677,10 +651,6 @@ if (@{$selected{'suppress-tags'}} && !@{$selected{tags}}) {
 
 # initialize display level settings; dies on error
 $PROFILE->display(@{$_}) for @display_level;
-
-$SIG{TERM} = \&interrupted;
-$SIG{INT} = \&interrupted;
-$SIG{QUIT} = \&interrupted;
 
 my @subjects;
 push(@subjects, @ARGV);
@@ -764,10 +734,7 @@ if ($pool->empty) {
     exit $exit_code;
 }
 
-$pool->process($PROFILE, \$exit_code, \%selected, $STATUS_FD, $OUTPUT);
-
-retrigger_signal()
-  if $received_signal;
+$pool->process($PROFILE, \$exit_code, \%selected, $OUTPUT);
 
 exit $exit_code;
 
@@ -961,63 +928,6 @@ sub default_jobs {
 
     # could be 2x
     return $cpus + 1;
-}
-
-sub END {
-
-    $SIG{INT} = 'DEFAULT';
-    $SIG{QUIT} = 'DEFAULT';
-
-    # Prevent LAB->close, $unpacker->kill_jobs etc. from affecting
-    # the exit code.
-    local $! = undef;
-    local $? = 0;
-    local $@ = undef;
-
-    my %already_closed;
-
-    for my $to_close (@CLOSE_AT_END) {
-
-        my ($fd, $filename) = @{$to_close};
-        my $fno = fileno($fd);
-
-        # Already closed?  Can happen with e.g.
-        #   --perf-output '&1' --status-log '&1'
-        next
-          unless defined $fno;
-
-        next
-          if $fno >= 0 && $already_closed{$fno}++;
-
-        eval {close $fd;};
-        if (my $err = $@) {
-            # Don't use L::Output here as it might be (partly) cleaned
-            # up.
-            warn encode_utf8("warning: closing ${filename} failed: $err\n");
-        }
-    }
-}
-
-sub _die_in_signal_handler {
-    die encode_utf8("N: Interrupted.\n");
-}
-
-sub retrigger_signal {
-    # Re-kill ourselves with the same signal to ensure that the exit
-    # code reflects that we died by a signal.
-    local $SIG{$received_signal} = \&_die_in_signal_handler;
-    $OUTPUT->debug_msg(2, "Retriggering signal SIG${received_signal}");
-    return kill($received_signal, $$);
-}
-
-sub interrupted {
-    my ($signal) = @_;
-
-    $received_signal = $signal;
-    $SIG{$received_signal} = 'DEFAULT';
-    print {$STATUS_FD} encode_utf8("ack-signal SIG${received_signal}\n");
-
-    return _die_in_signal_handler();
 }
 
 sub show_help {

lib/Lintian/Output.pm
@@ -38,11 +38,6 @@ Lintian::Output - Lintian messaging handling
 
     my $out = Lintian::Output->new;
 
-    $out->verbosity(-1);
-    $out->msg("Something interesting");
-    $out->v_msg("Something less interesting");
-    $out->debug_msg(3, "Something very specific");
-
 =head1 DESCRIPTION
 
 Lintian::Output is used for all interaction between lintian and the user.
@@ -63,15 +58,6 @@ The following fields impact the behavior of Lintian::Output.
 =item color
 
 =item colors
-
-=item debug
-
-If set to a positive integer, will enable all debug messages issued with
-a level lower or equal to its value.
-
-=item perf_debug
-
-=item perf_log_fd
 
 =item proc_id2tag_count
 
@@ -111,14 +97,11 @@ has colors => (
             'O' => 'bright_black',
         }
     });
-has perf_debug => (is => 'rw', default => 0);
-has perf_log_fd => (is => 'rw', default => sub { \*STDOUT });
 has proc_id2tag_count => (is => 'rw', default => sub { {} });
 has tag_display_limit => (is => 'rw', default => 4);
 has tty_hyperlinks => (is => 'rw', default => 0);
 has verbosity => (is => 'rw', default => 0);
 
-has debug => (is => 'rw', default => 0);
 has showdescription => (is => 'rw', default => sub { {} });
 
 has delimiter => (is => 'rw', default => '----');
@@ -168,38 +151,6 @@ sub v_msg {
       unless $self->verbosity;
 
     say encode_utf8("N: $_") for @args;
-
-    return;
-}
-
-sub debug_msg {
-    my ($self, $level, @args) = @_;
-
-    return
-      if $self->debug < $level;
-
-    say encode_utf8("N: $_") for @args;
-
-    return;
-}
-
-=item  C<perf_log(@args)>
-
-Like "v_msg", except output is possibly sent to a dedicated log
-file.
-
-Will output the strings given in @args, one per line.  The lines will
-not be prefixed.  Will do nothing unless perf_debug is set to a
-positive integer.
-
-=cut
-
-sub perf_log {
-    my ($self, @args) = @_;
-
-    return unless $self->perf_debug;
-
-    say { $self->perf_log_fd } encode_utf8($_) for @args;
 
     return;
 }

lib/Lintian/Pool.pm
@@ -39,6 +39,8 @@ use Lintian::Group;
 use Moo;
 use namespace::clean;
 
+const my $SPACE => q{ };
+
 const my $ANY_CHILD => -1;
 const my $WORLD_WRITABLE_FOLDER => oct(777);
 
@@ -148,7 +150,7 @@ Process the pool.
 =cut
 
 sub process{
-    my ($self, $PROFILE, $exit_code_ref, $option, $STATUS_FD, $OUTPUT)= @_;
+    my ($self, $PROFILE, $exit_code_ref, $option, $OUTPUT)= @_;
 
     my %override_count;
     my %ignored_overrides;
@@ -277,21 +279,17 @@ sub process{
             }
         }
 
-        # remove group files
-        $group->clean_lab($OUTPUT);
-
         my $total_raw_res = tv_interval($total_start);
         my $total_tres = sprintf('%.3fs', $total_raw_res);
 
-        if ($success) {
-            print {$STATUS_FD}
-              encode_utf8('complete ' . $group->name . " ($total_tres)\n");
-        } else {
-            print {$STATUS_FD}
-              encode_utf8('error ' . $group->name . " ($total_tres)\n");
-            ${$exit_code_ref} = 1;
-        }
+        my $status = $success ? 'complete' : 'error';
+        say {*STDERR}
+          encode_utf8($status . $SPACE . $group->name . " ($total_tres)")
+          if $option->{'status-log'};
         $OUTPUT->v_msg('Finished processing group ' . $group->name);
+
+        ${$exit_code_ref} = 1
+          unless $success;
     }
 
     # pass everything, in case some groups or processables have no hints

man/lintian.pod
@@ -483,13 +483,6 @@ Developer/Special usage options:
 
 Override lintian's warning when it is run with superuser privileges.
 
-=item B<--keep-lab>
-
-By default, temporary labs will be removed after lintian is finished.
-Specifying this options will leave the lab behind, which might be
-useful for debugging purposes.  You can find out where the temporary
-lab is located by running lintian with the B<--verbose> option.
-
 =item B<--packages-from-file> X
 
 The line is read as the path to a file to process (all whitespace is
@@ -499,37 +492,13 @@ If X is "-", Lintian will read the packages from STDIN.
 
 =item B<--perf-debug>
 
-Enable performance related debug logging.
+Enable performance related debug logging to STDERR.
 
 The data logged and the format used is subject to change with every
 release.
 
 Note that some of the information may also be available (possibly in
 a different format) with the B<--debug> option.
-
-=item B<--perf-output> OUTPUT
-
-Write performance related debug information to the specified file or
-file descriptor.  If OUTPUT starts with a '&' or '+', Lintian will
-handle OUTPUT specially.  Otherwise, Lintian will open the file
-denoted by OUTPUT for writing (truncating if it exists, creating it
-if it does not exist).
-
-If the first character of OUTPUT is a & and the rest of argument is a
-number N, then lintian attempts to write it to the file descriptor
-with the number N.  Said file descriptor must be open for writing.
-E.g I<&2> makes Lintian write the performance logging to STDERR.
-
-If the first character of OUTPUT is a +, Lintian will append to the
-file rather than truncating it.  In this case, the file name is OUTPUT
-with initial "+" character removed.  E.g. I<+my-file> makes Lintian
-append to I<my-file>
-
-If Lintian should write the output to a file starting with a literal
-'&' or '+', then simply prefix it with "./" (e.g. "+my-file" becomes
-"./+my-file").
-
-If this option omitted, Lintian will default to using STDOUT.
 
 =back
 

commit 07db1a633721fb91fb6013fe789aac3864d2d961

Group.pm: fix --perf-debug to actually emit performance CSV
The --perf-debug option was declared in bin/lintian and documented in the
manual, but the timing gates in Group::process checked the stale option
name 'perf-output' (left over from a removed log-file mechanism), so the
flag never produced any output.

Point both gates at the option that actually exists ('perf-debug') so the
per-check and total-group-check CSV timing lines are emitted to STDERR.

Fixes: commit 697de993f07145e9f4c00aaec3a87514c4bca29c

AI-assisted: yes, opencode + scaleway/deepseek-v4-flash-0731

lib/Lintian/Group.pm
@@ -294,7 +294,7 @@ sub process {
             say {*STDERR} encode_utf8("Check $name for $procid done ($tres)")
               if $option->{debug};
             say {*STDERR} encode_utf8("$procid,check/$name,$raw_res")
-              if $option->{'perf-output'};
+              if $option->{'perf-debug'};
         }
 
         my %context_tracker;
@@ -463,7 +463,7 @@ sub process {
       encode_utf8('Checking all of group ' . $self->name . " done ($tres)")
       if $option->{debug};
     say {*STDERR} encode_utf8($self->name . ",total-group-check,$raw_res")
-      if $option->{'perf-output'};
+      if $option->{'perf-debug'};
 
     if ($option->{'debug'} > 2) {
 